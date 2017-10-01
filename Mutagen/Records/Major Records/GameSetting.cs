11dd5240318524b70792d03ea0c10cf22a6f8508
Justin Swanson <justin.c.swanson@gmail.com>
2017-09-30 20:36:46 -0500

    GameSetting parse work

--- Mutagen/Records/Major Records/GameSetting.cs
+++ Mutagen/Records/Major Records/GameSetting.cs
@@ -1,12 +1,67 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Mutagen.Binary;
+using Mutagen.Internals;
+using Noggog.Notifying;
+using System;
+using System.IO;
 
 namespace Mutagen
 {
     public partial class GameSetting
     {
+        protected abstract char TriggerChar { get; }
+
+        partial void CustomCtor()
+        {
+            this.EditorID_Property.Subscribe(CorrectEDID, fireInitial: false);
+        }
+
+        private void CorrectEDID(Change<string> change)
+        {
+            if (change.New.Length == 0)
+            {
+                this.EditorID = string.Empty + this.TriggerChar;
+            }
+            else if (!this.TriggerChar.Equals(change.New[0]))
+            {
+                this.EditorID = this.TriggerChar + change.New;
+            }
+        }
+
+        public static (GameSetting Object, GameSetting_ErrorMask ErrorMask) Create_Binary(
+            BinaryReader reader,
+            bool doMasks)
+        {
+            var initialPos = reader.BaseStream.Position;
+            reader.BaseStream.Position += 20;
+            if (!MajorRecord_Registration.EDID_HEADER.Equals(HeaderTranslation.GetNextSubRecordType(reader, out var contentLength)))
+            {
+                throw new ArgumentException($"EDID was not located in expected position: {reader.BaseStream.Position}");
+            }
+            var edid = StringBinaryTranslation.Instance.Parse(
+                reader,
+                contentLength,
+                doMasks,
+                out var err);
+            if (err != null)
+            {
+                throw new ArgumentException($"Error parsing EDID: {err}");
+            }
+            if (edid.Failed || edid.Value.Length == 0)
+            {
+                throw new ArgumentException("No EDID parsed.");
+            }
+            reader.BaseStream.Position = initialPos;
+            switch (edid.Value[0])
+            {
+                case GameSettingInt.TRIGGER_CHAR:
+                    return GameSettingInt.Create_Binary(reader, doMasks);
+                case GameSettingString.TRIGGER_CHAR:
+                    return GameSettingString.Create_Binary(reader, doMasks);
+                case GameSettingFloat.TRIGGER_CHAR:
+                    return GameSettingFloat.Create_Binary(reader, doMasks);
+                default:
+                    throw new ArgumentException($"Unknown game setting type: {edid.Value[0]}");
+            }
+        }
     }
 }
---
+++ Mutagen/Records/Major Records/GameSettingInt.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mutagen
+{
+    public partial class GameSettingInt
+    {
+        public const char TRIGGER_CHAR = 'i';
+        protected override char TriggerChar => TRIGGER_CHAR;
+    }
+}
