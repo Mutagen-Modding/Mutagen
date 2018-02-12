f961168d6e93a6c63b13903fbc27fbc1666dcea7
Justin Swanson <justin.c.swanson@gmail.com>
2018-02-11 19:05:13 -0600

    Added unit test for an issue that needs addressing later

---
+++ Mutagen.Bethesda.Tests/Misc_Tests.cs
@@ -0,0 +1,24 @@
+﻿using Loqui;
+using Mutagen.Bethesda.Internals;
+using Mutagen.Bethesda.Oblivion.Internals;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Mutagen.Bethesda.Tests
+{
+    public class Misc_Tests
+    {
+        [Fact]
+        public void GroupErrTest()
+        {
+            Group_ErrorMask<NPC_ErrorMask> group_ErrorMask = new Group_ErrorMask<NPC_ErrorMask>();
+            group_ErrorMask.SetNthMask(
+                (int)Group_FieldIndex.Items,
+                new MaskItem<Exception, IEnumerable<MaskItem<Exception, NPC_ErrorMask>>>(null, null));
+        }
+    }
+}
