a37c46095d721fe6552cf195ddc5be668e51551e
Justin Swanson <justin.c.swanson@gmail.com>
2018-09-25 15:43:28 -0500

    FormID ModID truncating if the index is larger than expected.  Bugfix for criteria for looking at modlist when linking

--- Mutagen.Bethesda/Links/FormIDLink.cs
+++ Mutagen.Bethesda/Links/FormIDLink.cs
@@ -70,8 +70,13 @@ namespace Mutagen.Bethesda
                 item = default(T);
                 return false;
             }
+            if (unlinkedForm.Value.ModID != ModID.Zero
+                && unlinkedForm.Value.ModID.ID > sourceMod.MasterReferences.Count)
+            {
+                unlinkedForm = new FormID(ModID.Zero, unlinkedForm.Value.ID);
+            }
             M mod;
-            if (modList != null && unlinkedForm.Value.ModID != ModID.Zero)
+            if (modList != null && unlinkedForm.Value.ModID.ID <= sourceMod.MasterReferences.Count)
             {
                 if (!sourceMod.MasterReferences.TryGet(unlinkedForm.Value.ModID.ID, out var masterRef)
                     || !ModKey.TryFactory(masterRef.Master, out var modKey)
