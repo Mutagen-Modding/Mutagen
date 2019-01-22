c553521f3ce4fc657b0f4f0e14b93e7d4280c870
Justin Swanson <justin.c.swanson@gmail.com>
2019-01-21 23:42:47 -0600

    Mods export as version 1 by default

---
+++ Mutagen.Bethesda.Oblivion/Records/Header/Header.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mutagen.Bethesda.Oblivion
+{
+    public partial class Header
+    {
+        partial void CustomCtor()
+        {
+            this.Version = 1.0f;
+        }
+    }
+}
