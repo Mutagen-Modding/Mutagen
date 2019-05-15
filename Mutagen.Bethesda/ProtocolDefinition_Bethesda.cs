214ba96aca0fb9a30bcfed45bd338347a6573e6a
Justin Swanson <justin.c.swanson@gmail.com>
2019-05-15 17:47:46 -0500

    Loqui API updates

--- Mutagen.Bethesda/ProtocolDefinition_Bethesda.cs
+++ Mutagen.Bethesda/ProtocolDefinition_Bethesda.cs
@@ -6,7 +6,8 @@ namespace Loqui
     public class ProtocolDefinition_Bethesda : IProtocolRegistration
     {
         public readonly static ProtocolKey ProtocolKey = new ProtocolKey("Bethesda");
-        public void Register()
+        void IProtocolRegistration.Register() => Register();
+        public static void Register()
         {
             LoquiRegistration.Register(Mutagen.Bethesda.Internals.MajorRecord_Registration.Instance);
             LoquiRegistration.Register(Mutagen.Bethesda.Internals.MasterReference_Registration.Instance);
