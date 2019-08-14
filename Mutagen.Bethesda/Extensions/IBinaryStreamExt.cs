894300a1aadb9c46b8e9aee762aaefe504a9b07d
Justin Swanson <justin.c.swanson@gmail.com>
2019-08-14 16:58:28 -0500

    ReadColor span parsing reuse.  Alpha assumed as 4th byte

--- Mutagen.Bethesda/Extensions/IBinaryStreamExt.cs
+++ Mutagen.Bethesda/Extensions/IBinaryStreamExt.cs
@@ -12,22 +12,13 @@ namespace Mutagen.Bethesda
     {
         public static Color ReadColor(this IBinaryReadStream stream)
         {
-            var ret = Color.FromArgb(
-                alpha: 0,
-                red: stream.ReadUInt8(),
-                green: stream.ReadUInt8(),
-                blue: stream.ReadUInt8());
-            if (stream.Remaining > 0)
-            {
-                stream.ReadUInt8();
-            }
-            return ret;
+            return ReadColor(stream.ReadSpan(stream.Remaining >= 4 ? 4 : 3));
         }
 
         public static Color ReadColor(this ReadOnlySpan<byte> span)
         {
             return Color.FromArgb(
-                alpha: 0,
+                alpha: span.Length >= 4 ? span[3] : 0,
                 red: span[0],
                 green: span[1],
                 blue: span[2]);
