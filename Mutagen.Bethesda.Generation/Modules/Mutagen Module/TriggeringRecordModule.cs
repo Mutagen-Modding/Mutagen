699a678dfe35a0300f92459cbd4b138260958121
Justin Swanson <justin.c.swanson@gmail.com>
2019-11-13 17:31:37 -0600

    Fix for triggering record module to only look at current gen

--- Mutagen.Bethesda.Generation/Modules/Mutagen Module/TriggeringRecordModule.cs
+++ Mutagen.Bethesda.Generation/Modules/Mutagen Module/TriggeringRecordModule.cs
@@ -282,7 +282,7 @@ namespace Mutagen.Bethesda.Generation
                 }
                 else if (loqui.RefType == LoquiType.LoquiRefType.Interface)
                 {
-                    var implementingObjs = obj.ProtoGen.Gen.ObjectGenerations
+                    var implementingObjs = obj.ProtoGen.ObjectGenerationsByID.Values
                         .Where(o => o.Interfaces.ContainsAtLeast(loqui.GetterInterface, LoquiInterfaceType.Direct)
                             || o.Interfaces.ContainsAtLeast(loqui.SetterInterface, LoquiInterfaceType.Direct))
                         .ToArray();
