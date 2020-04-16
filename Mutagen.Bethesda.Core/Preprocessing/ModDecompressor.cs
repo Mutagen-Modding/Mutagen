6894e74286a57260cdaec3267d38aeec8f49ec4c
Justin Swanson <justin.c.swanson@gmail.com>
2020-04-16 17:01:38 -0500

    ModDecompressor simple usings

--- Mutagen.Bethesda.Core/Preprocessing/ModDecompressor.cs
+++ Mutagen.Bethesda.Core/Preprocessing/ModDecompressor.cs
@@ -30,89 +30,84 @@ namespace Mutagen.Bethesda.Preprocessing
             RecordInterest? interest = null)
         {
             var meta = GameConstants.Get(gameMode);
-            using (var inputStream = new MutagenBinaryReadStream(streamCreator(), meta, masterReferences: null))
-            {
-                using (var inputStreamJumpback = new MutagenBinaryReadStream(streamCreator(), meta))
+            using var inputStream = new MutagenBinaryReadStream(streamCreator(), meta, masterReferences: null);
+            using var inputStreamJumpback = new MutagenBinaryReadStream(streamCreator(), meta);
+            using var writer = new System.IO.BinaryWriter(outputStream, Encoding.Default, leaveOpen: true);
+
+            long runningDiff = 0;
+            var fileLocs = RecordLocator.GetFileLocations(
+                inputStream,
+                interest: interest,
+                additionalCriteria: (stream, recType, len) =>
                 {
-                    using (var writer = new System.IO.BinaryWriter(outputStream, Encoding.Default, leaveOpen: true))
-                    {
-                        long runningDiff = 0;
-                        var fileLocs = RecordLocator.GetFileLocations(
-                            inputStream,
-                            interest: interest,
-                            additionalCriteria: (stream, recType, len) =>
-                            {
-                                return stream.GetMajorRecord().IsCompressed;
-                            });
+                    return stream.GetMajorRecord().IsCompressed;
+                });
 
-                        // Construct group length container for later use
-                        Dictionary<long, (uint Length, long Offset)> grupMeta = new Dictionary<long, (uint Length, long Offset)>();
-                        inputStream.Position = 0;
-                        while (!inputStream.Complete)
-                        {
-                            // Import until next listed major record
-                            long noRecordLength;
-                            if (fileLocs.ListedRecords.TryGetInDirection(
-                                inputStream.Position,
-                                higher: true,
-                                result: out var nextRec))
-                            {
-                                var recordLocation = fileLocs.ListedRecords.Keys[nextRec.Key];
-                                noRecordLength = recordLocation - inputStream.Position;
-                            }
-                            else
-                            {
-                                noRecordLength = inputStream.Length - inputStream.Position;
-                            }
-                            inputStream.WriteTo(outputStream, (int)noRecordLength);
-
-                            // If complete overall, return
-                            if (inputStream.Complete) break;
-                            var majorMeta = inputStream.ReadMajorRecord();
-                            var len = majorMeta.ContentLength;
-                            using (var frame = MutagenFrame.ByLength(
-                                reader: inputStream,
-                                length: len))
-                            {
-                                // Decompress
-                                var decompressed = frame.Decompress();
-                                var decompressedLen = decompressed.TotalLength;
-                                var lengthDiff = decompressedLen - len;
-                                var majorMetaSpan = majorMeta.Span.ToArray();
-
-                                // Write major Meta
-                                var writableMajorMeta = meta.MajorRecordWritable(majorMetaSpan.AsSpan());
-                                writableMajorMeta.IsCompressed = false;
-                                writableMajorMeta.RecordLength = (uint)(len + lengthDiff);
-                                writer.Write(majorMetaSpan);
-                                writer.Write(decompressed.ReadRemaining());
-
-                                // If no difference in lengths, move on
-                                if (lengthDiff == 0) continue;
-
-                                // Modify parent group lengths
-                                foreach (var grupLoc in fileLocs.GetContainingGroupLocations(nextRec.Value.FormID))
-                                {
-                                    if (!grupMeta.TryGetValue(grupLoc, out var loc))
-                                    {
-                                        loc.Offset = runningDiff;
-                                        inputStreamJumpback.Position = grupLoc + 4;
-                                        loc.Length = inputStreamJumpback.ReadUInt32();
-                                    }
-                                    grupMeta[grupLoc] = ((uint)(loc.Length + lengthDiff), loc.Offset);
-                                }
-                                runningDiff += lengthDiff;
-                            }
-                        }
-
-                        foreach (var item in grupMeta)
-                        {
-                            var grupLoc = item.Key;
-                            outputStream.Position = grupLoc + 4 + item.Value.Offset;
-                            writer.Write(item.Value.Length);
-                        }
-                    }
+            // Construct group length container for later use
+            Dictionary<long, (uint Length, long Offset)> grupMeta = new Dictionary<long, (uint Length, long Offset)>();
+            inputStream.Position = 0;
+            while (!inputStream.Complete)
+            {
+                // Import until next listed major record
+                long noRecordLength;
+                if (fileLocs.ListedRecords.TryGetInDirection(
+                    inputStream.Position,
+                    higher: true,
+                    result: out var nextRec))
+                {
+                    var recordLocation = fileLocs.ListedRecords.Keys[nextRec.Key];
+                    noRecordLength = recordLocation - inputStream.Position;
                 }
+                else
+                {
+                    noRecordLength = inputStream.Length - inputStream.Position;
+                }
+                inputStream.WriteTo(outputStream, (int)noRecordLength);
+
+                // If complete overall, return
+                if (inputStream.Complete) break;
+                var majorMeta = inputStream.ReadMajorRecord();
+                var len = majorMeta.ContentLength;
+                using (var frame = MutagenFrame.ByLength(
+                    reader: inputStream,
+                    length: len))
+                {
+                    // Decompress
+                    var decompressed = frame.Decompress();
+                    var decompressedLen = decompressed.TotalLength;
+                    var lengthDiff = decompressedLen - len;
+                    var majorMetaSpan = majorMeta.Span.ToArray();
+
+                    // Write major Meta
+                    var writableMajorMeta = meta.MajorRecordWritable(majorMetaSpan.AsSpan());
+                    writableMajorMeta.IsCompressed = false;
+                    writableMajorMeta.RecordLength = (uint)(len + lengthDiff);
+                    writer.Write(majorMetaSpan);
+                    writer.Write(decompressed.ReadRemaining());
+
+                    // If no difference in lengths, move on
+                    if (lengthDiff == 0) continue;
+
+                    // Modify parent group lengths
+                    foreach (var grupLoc in fileLocs.GetContainingGroupLocations(nextRec.Value.FormID))
+                    {
+                        if (!grupMeta.TryGetValue(grupLoc, out var loc))
+                        {
+                            loc.Offset = runningDiff;
+                            inputStreamJumpback.Position = grupLoc + 4;
+                            loc.Length = inputStreamJumpback.ReadUInt32();
+                        }
+                        grupMeta[grupLoc] = ((uint)(loc.Length + lengthDiff), loc.Offset);
+                    }
+                    runningDiff += lengthDiff;
+                }
+            }
+
+            foreach (var item in grupMeta)
+            {
+                var grupLoc = item.Key;
+                outputStream.Position = grupLoc + 4 + item.Value.Offset;
+                writer.Write(item.Value.Length);
             }
         }
     }
