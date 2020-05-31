02568eaf278b82ebfaffbd7e98b28c6731668067
Justin Swanson <justin.c.swanson@gmail.com>
2020-05-31 16:01:56 -0500

    PlacedObject major flag alternates

--- Mutagen.Bethesda/Skyrim/Records/Major Records/PlacedObject.cs
+++ Mutagen.Bethesda/Skyrim/Records/Major Records/PlacedObject.cs
@@ -16,6 +16,148 @@ namespace Mutagen.Bethesda.Skyrim
             Open,
             OpenByDefault,
         }
+
+        [Flags]
+        public enum DefaultMajorFlag : uint
+        {
+            Persistent = 0x0000_0400,
+            InitiallyDisabled = 0x0000_0800,
+            IsFullLod = 0x0001_0000,
+            FilterCollisionGeometry = 0x0400_0000,
+            BoundingBoxCollisionGeometry = 0x0800_0000,
+            ReflectedByAutoWater = 0x1000_0000,
+            DontHavokSettle = 0x2000_0000,
+            Ground = 0x4000_0000,
+            Multibound = 0x8000_0000,
+        }
+
+        /// <summary>
+        /// Used when Placed Object refers to:
+        /// [Activator, Static, Tree]
+        /// </summary>
+        [Flags]
+        public enum StaticMajorFlag : uint
+        {
+            HiddenFromLocalMap = 0x0000_0200,
+            Persistent = 0x0000_0400,
+            InitiallyDisabled = 0x0000_0800,
+            VisibleWhenDistant = 0x0000_8000,
+            IsFullLod = 0x0001_0000,
+            FilterCollisionGeometry = 0x0400_0000,
+            BoundingBoxCollisionGeometry = 0x0800_0000,
+            ReflectedByAutoWater = 0x1000_0000,
+            DontHavokSettle = 0x2000_0000,
+            NoRespawn = 0x4000_0000,
+            Multibound = 0x8000_0000,
+        }
+
+        /// <summary>
+        /// Used when Placed Object refers to: [Container]
+        /// </summary>
+        [Flags]
+        public enum ContainerMajorFlag : uint
+        {
+            Persistent = 0x0000_0400,
+            InitiallyDisabled = 0x0000_0800,
+            VisibleWhenDistant = 0x0000_8000,
+            IsFullLod = 0x0001_0000,
+            NoAiAcquire = 0x0200_0000,
+            FilterCollisionGeometry = 0x0400_0000,
+            BoundingBoxCollisionGeometry = 0x0800_0000,
+            ReflectedByAutoWater = 0x1000_0000,
+            DontHavokSettle = 0x2000_0000,
+            Ground = 0x4000_0000,
+            Multibound = 0x8000_0000,
+        }
+
+        /// <summary>
+        /// Used when Placed Object refers to: [Door]
+        /// </summary>
+        [Flags]
+        public enum DoorMajorFlag : uint
+        {
+            HiddenFromLocalMap = 0x0000_0040,
+            Inaccessible = 0x0000_0100,
+            Persistent = 0x0000_0400,
+            InitiallyDisabled = 0x0000_0800,
+            IsFullLod = 0x0001_0000,
+            FilterCollisionGeometry = 0x0400_0000,
+            BoundingBoxCollisionGeometry = 0x0800_0000,
+            ReflectedByAutoWater = 0x1000_0000,
+            DontHavokSettle = 0x2000_0000,
+            NoRespawn = 0x4000_0000,
+            Multibound = 0x8000_0000,
+        }
+
+        /// <summary>
+        /// Used when Placed Object refers to: [Light]
+        /// </summary>
+        [Flags]
+        public enum LightMajorFlag : uint
+        {
+            DoesNotLightWater = 0x0000_0100,
+            CastsShadows = 0x0000_0200,
+            Persistent = 0x0000_0400,
+            InitiallyDisabled = 0x0000_0800,
+            NeverFades = 0x0001_0000,
+            DoesNotLightLandscape = 0x0002_0000,
+            NoAiAcquire = 0x0200_0000,
+            ReflectedByAutoWater = 0x1000_0000,
+            DontHavokSettle = 0x2000_0000,
+            NoRespawn = 0x4000_0000,
+            Multibound = 0x8000_0000,
+        }
+
+        /// <summary>
+        /// Used when Placed Object refers to: [MoveableStatic]
+        /// </summary>
+        [Flags]
+        public enum MoveableStaticMajorFlag : uint
+        {
+            MotionBlur = 0x0000_0200,
+            Persistent = 0x0000_0400,
+            InitiallyDisabled = 0x0000_0800,
+            IsFullLod = 0x0001_0000,
+            FilterCollisionGeometry = 0x0400_0000,
+            BoundingBoxCollisionGeometry = 0x0800_0000,
+            ReflectedByAutoWater = 0x1000_0000,
+            DontHavokSettle = 0x2000_0000,
+            NoRespawn = 0x4000_0000,
+            Multibound = 0x8000_0000,
+        }
+
+        /// <summary>
+        /// Used when Placed Object refers to: [AddonNode]
+        /// </summary>
+        [Flags]
+        public enum AddonNodeMajorFlag : uint
+        {
+            Persistent = 0x0000_0400,
+            InitiallyDisabled = 0x0000_0800,
+            IsFullLod = 0x0001_0000,
+            ReflectedByAutoWater = 0x1000_0000,
+            DontHavokSettle = 0x2000_0000,
+            NoRespawn = 0x4000_0000,
+            Multibound = 0x8000_0000,
+        }
+
+        /// <summary>
+        /// Used when Placed Object refers to:
+        /// [Ingestible, Scroll, Ammunition, Ingredient, Key, MiscItem, SoulGem, Weapon]
+        /// </summary>
+        [Flags]
+        public enum ItemMajorFlag : uint
+        {
+            Persistent = 0x0000_0400,
+            InitiallyDisabled = 0x0000_0800,
+            VisibleWhenDistant = 0x0000_8000,
+            IsFullLod = 0x0001_0000,
+            NoAiAcquire = 0x0200_0000,
+            ReflectedByAutoWater = 0x1000_0000,
+            DontHavokSettle = 0x2000_0000,
+            NoRespawn = 0x4000_0000,
+            Multibound = 0x8000_0000,
+        }
     }
 
     namespace Internals
