256b9e4ce2b43df5ef61084e715377536e38c7cc
Justin Swanson <justin.c.swanson@gmail.com>
2020-07-17 21:14:10 -0500

    Missing API refactors

--- Mutagen.Bethesda.Core/Preprocessing/ModRecordSorter.cs
+++ Mutagen.Bethesda.Core/Preprocessing/ModRecordSorter.cs
@@ -34,10 +34,6 @@ namespace Mutagen.Bethesda.Preprocessing
                     if (inputStream.Complete) return;
 
                     var groupMeta = inputStream.GetGroup();
-                    if (!groupMeta.IsGroup)
-                    {
-                        throw new ArgumentException();
-                    }
 
                     var storage = new Dictionary<FormID, List<ReadOnlyMemorySlice<byte>>>();
                     using (var grupFrame = new MutagenFrame(inputStream).SpawnWithLength(groupMeta.TotalLength))
@@ -49,8 +45,7 @@ namespace Mutagen.Bethesda.Preprocessing
                             MajorRecordHeader majorMeta = inputStream.GetMajorRecord();
                             storage.TryCreateValue(rec.FormID).Add(inputStream.ReadMemory(checked((int)majorMeta.TotalLength), readSafe: true));
                             if (grupFrame.Complete) continue;
-                            GroupHeader subGroupMeta = inputStream.GetGroup();
-                            if (subGroupMeta.IsGroup)
+                            if (inputStream.TryGetGroup(out var subGroupMeta))
                             {
                                 storage.TryCreateValue(rec.FormID).Add(inputStream.ReadMemory(checked((int)subGroupMeta.TotalLength), readSafe: true));
                             }
