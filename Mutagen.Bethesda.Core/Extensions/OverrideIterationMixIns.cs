4862f5b3b121db354d0d1b9ee2ec0b4cb84677e4
Justin Swanson <justin.c.swanson@gmail.com>
2020-08-03 08:26:45 -0500

    OverrideIterationMixIn

---
+++ Mutagen.Bethesda.Core/Extensions/OverrideIterationMixIns.cs
@@ -0,0 +1,68 @@
+﻿using Noggog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Mutagen.Bethesda
+{
+    public static class OverrideIterationMixIns
+    {
+        public static IEnumerable<TMajor> WinningOverrides<TMajor, TMod>(this LoadOrder<TMod> loadOrder)
+            where TMod : class, IModGetter
+            where TMajor : class, IMajorRecordCommonGetter
+        {
+            return loadOrder.PriorityOrder.WinningOverrides<TMajor>();
+        }
+        
+        public static IEnumerable<IMajorRecordCommonGetter> WinningOverrides<TMod>(this LoadOrder<TMod> loadOrder, Type type)
+            where TMod : class, IModGetter
+        {
+            return loadOrder.PriorityOrder.WinningOverrides(type);
+        }
+
+        public static IEnumerable<TMajor> WinningOverrides<TMajor>(this IEnumerable<IModListing<IModGetter>> priorityOrder)
+            where TMajor : class, IMajorRecordCommonGetter
+        {
+            return priorityOrder
+                .Select(l => l.Mod)
+                .NotNull()
+                .WinningOverrides<TMajor>();
+        }
+
+        public static IEnumerable<IMajorRecordCommonGetter> WinningOverrides(this IEnumerable<IModListing<IModGetter>> priorityOrder, Type type)
+        {
+            return priorityOrder
+                .Select(l => l.Mod)
+                .NotNull()
+                .WinningOverrides(type);
+        }
+
+        public static IEnumerable<TMajor> WinningOverrides<TMajor>(this IEnumerable<IModGetter> priorityOrder)
+            where TMajor : class, IMajorRecordCommonGetter
+        {
+            var passedRecords = new HashSet<FormKey>();
+            foreach (var mod in priorityOrder)
+            {
+                foreach (var record in mod.EnumerateMajorRecords<TMajor>())
+                {
+                    if (!passedRecords.Add(record.FormKey)) continue;
+                    yield return record;
+                }
+            }
+        }
+
+        public static IEnumerable<IMajorRecordCommonGetter> WinningOverrides(this IEnumerable<IModGetter> priorityOrder, Type type)
+        {
+            var passedRecords = new HashSet<FormKey>();
+            foreach (var mod in priorityOrder)
+            {
+                foreach (var record in mod.EnumerateMajorRecords(type))
+                {
+                    if (!passedRecords.Add(record.FormKey)) continue;
+                    yield return record;
+                }
+            }
+        }
+    }
+}
---
+++ Mutagen.Bethesda.UnitTests/OverrideIteration_Tests.cs
@@ -0,0 +1,36 @@
+﻿using Mutagen.Bethesda.Oblivion;
+using Noggog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace Mutagen.Bethesda.UnitTests
+{
+    public class OverrideIteration_Tests
+    {
+        [Fact]
+        public void WinningOverrides_Typical()
+        {
+            var master = new OblivionMod(new ModKey("Base", ModType.Master));
+            var baseNpc = master.Npcs.AddNew();
+            var otherMasterNpc = master.Npcs.AddNew();
+            var plugin = new OblivionMod(new ModKey("Plugin", ModType.Plugin));
+            var pluginNpc = plugin.Npcs.AddNew();
+            var overrideNpc = (Npc)baseNpc.DeepCopy();
+            plugin.Npcs.RecordCache.Set(overrideNpc);
+            overrideNpc.Name = "Override";
+
+            var winningOverrides = plugin.AsEnumerable().And(master)
+                .WinningOverrides<Npc>()
+                .ToDictionary(n => n.FormKey);
+
+            Assert.Equal(3, winningOverrides.Count);
+            Assert.Same(otherMasterNpc, winningOverrides[otherMasterNpc.FormKey]);
+            Assert.Same(pluginNpc, winningOverrides[pluginNpc.FormKey]);
+            Assert.Equal(overrideNpc.FormKey, baseNpc.FormKey);
+            Assert.Same(overrideNpc, winningOverrides[baseNpc.FormKey]);
+        }
+    }
+}
