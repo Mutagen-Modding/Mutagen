c12572b604c589135075161199bfecd1b0d45d09
Justin Swanson <justin.c.swanson@gmail.com>
2020-08-08 07:05:05 -0500

    Made StringsFolderLookupOverlay lazier

--- Mutagen.Bethesda.Core/String Lookup/StringsFolderLookupOverlay.cs
+++ Mutagen.Bethesda.Core/String Lookup/StringsFolderLookupOverlay.cs
@@ -13,55 +13,79 @@ namespace Mutagen.Bethesda
 {
     public class StringsFolderLookupOverlay : IStringsFolderLookup
     {
-        private readonly Dictionary<Language, Lazy<IStringsLookup>> _strings = new Dictionary<Language, Lazy<IStringsLookup>>();
-        private readonly Dictionary<Language, Lazy<IStringsLookup>> _dlstrings = new Dictionary<Language, Lazy<IStringsLookup>>();
-        private readonly Dictionary<Language, Lazy<IStringsLookup>> _ilstrings = new Dictionary<Language, Lazy<IStringsLookup>>();
+        private Lazy<DictionaryBundle> _dictionaries;
 
-        public bool Empty =>
-            _strings.Count == 0 
-            && _dlstrings.Count == 0
-            && _ilstrings.Count == 0;
-
-        private StringsFolderLookupOverlay()
+        class DictionaryBundle
         {
+            public readonly Dictionary<Language, Lazy<IStringsLookup>> Strings = new Dictionary<Language, Lazy<IStringsLookup>>();
+            public readonly Dictionary<Language, Lazy<IStringsLookup>> DlStrings = new Dictionary<Language, Lazy<IStringsLookup>>();
+            public readonly Dictionary<Language, Lazy<IStringsLookup>> IlStrings = new Dictionary<Language, Lazy<IStringsLookup>>();
+
+            public bool Empty =>
+                Strings.Count == 0
+                && DlStrings.Count == 0
+                && IlStrings.Count == 0;
+
+            public Dictionary<Language, Lazy<IStringsLookup>> Get(StringsSource source)
+            {
+                return source switch
+                {
+                    StringsSource.Normal => Strings,
+                    StringsSource.IL => IlStrings,
+                    StringsSource.DL => DlStrings,
+                    _ => throw new NotImplementedException(),
+                };
+            }
+        }
+
+        public bool Empty => _dictionaries.Value.Empty;
+
+        private StringsFolderLookupOverlay(Lazy<DictionaryBundle> instantiator)
+        {
+            _dictionaries = instantiator;
         }
 
         public static StringsFolderLookupOverlay TypicalFactory(string dataPath, StringsReadParameters? instructions, ModKey modKey)
         {
-            var ret = new StringsFolderLookupOverlay();
             var stringsFolderPath = instructions?.StringsFolderOverride;
             if (stringsFolderPath == null)
             {
                 stringsFolderPath = Path.Combine(dataPath, "Strings");
             }
-            if (stringsFolderPath.Value.Exists)
-            {
-                foreach (var file in stringsFolderPath.Value.Info.EnumerateFiles($"{modKey.Name}*{StringsUtility.StringsFileExtension}"))
+            return new StringsFolderLookupOverlay(new Lazy<DictionaryBundle>(
+                isThreadSafe: true,
+                valueFactory: () =>
                 {
-                    if (!StringsUtility.TryRetrieveInfoFromString(file.Name, out var type, out var lang, out _)) continue;
-                    var dict = ret.Get(type);
-                    dict[lang] = new Lazy<IStringsLookup>(() => new StringsLookupOverlay(file.FullName, type), LazyThreadSafetyMode.ExecutionAndPublication);
-                }
-            }
-            foreach (var bsaFile in Directory.EnumerateFiles(dataPath, "*.bsa"))
-            {
-                var bsaReader = BSAReader.Load(new AbsolutePath(bsaFile, skipValidation: true));
-                foreach (var item in bsaReader.Files)
-                {
-                    if (!StringsUtility.TryRetrieveInfoFromString(Path.GetFileName(item.Path.ToString()), out var type, out var lang, out var modName)) continue;
-                    if (!MemoryExtensions.Equals(modKey.Name, modName, StringComparison.OrdinalIgnoreCase)) continue;
-                    var dict = ret.Get(type);
-                    if (dict.ContainsKey(lang)) continue;
-                    dict[lang] = new Lazy<IStringsLookup>(() =>
+                    var bundle = new DictionaryBundle();
+                    if (stringsFolderPath.Value.Exists)
                     {
-                        byte[] bytes = new byte[item.Size];
-                        using var stream = new MemoryStream(bytes);
-                        item.CopyDataTo(stream).AsTask().Wait();
-                        return new StringsLookupOverlay(bytes, type);
-                    }, LazyThreadSafetyMode.ExecutionAndPublication);
-                }
-            }
-            return ret;
+                        foreach (var file in stringsFolderPath.Value.Info.EnumerateFiles($"{modKey.Name}*{StringsUtility.StringsFileExtension}"))
+                        {
+                            if (!StringsUtility.TryRetrieveInfoFromString(file.Name, out var type, out var lang, out _)) continue;
+                            var dict = bundle.Get(type);
+                            dict[lang] = new Lazy<IStringsLookup>(() => new StringsLookupOverlay(file.FullName, type), LazyThreadSafetyMode.ExecutionAndPublication);
+                        }
+                    }
+                    foreach (var bsaFile in Directory.EnumerateFiles(dataPath, "*.bsa"))
+                    {
+                        var bsaReader = BSAReader.Load(new AbsolutePath(bsaFile, skipValidation: true));
+                        foreach (var item in bsaReader.Files)
+                        {
+                            if (!StringsUtility.TryRetrieveInfoFromString(Path.GetFileName(item.Path.ToString()), out var type, out var lang, out var modName)) continue;
+                            if (!MemoryExtensions.Equals(modKey.Name, modName, StringComparison.OrdinalIgnoreCase)) continue;
+                            var dict = bundle.Get(type);
+                            if (dict.ContainsKey(lang)) continue;
+                            dict[lang] = new Lazy<IStringsLookup>(() =>
+                            {
+                                byte[] bytes = new byte[item.Size];
+                                using var stream = new MemoryStream(bytes);
+                                item.CopyDataTo(stream).AsTask().Wait();
+                                return new StringsLookupOverlay(bytes, type);
+                            }, LazyThreadSafetyMode.ExecutionAndPublication);
+                        }
+                    }
+                    return bundle;
+                }));
         }
 
         /// <inheritdoc />
@@ -76,16 +100,7 @@ namespace Mutagen.Bethesda
             return lookup.Value.TryLookup(key, out str);
         }
 
-        public Dictionary<Language, Lazy<IStringsLookup>> Get(StringsSource source)
-        {
-            return source switch
-            {
-                StringsSource.Normal => _strings,
-                StringsSource.IL => _ilstrings,
-                StringsSource.DL => _dlstrings,
-                _ => throw new NotImplementedException(),
-            };
-        }
+        public Dictionary<Language, Lazy<IStringsLookup>> Get(StringsSource source) => _dictionaries.Value.Get(source);
 
         public TranslatedString CreateString(StringsSource source, uint key)
         {
