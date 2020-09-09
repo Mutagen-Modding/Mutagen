14c77c8e4f4526a0deb08175ecd2708223ce23f9
Justin Swanson <justin.c.swanson@gmail.com>
2020-09-09 00:28:46 -0500

    Adjusted ILink resolution extensions to properly pass along their generic

--- Mutagen.Bethesda.Core/Links/ILink.cs
+++ Mutagen.Bethesda.Core/Links/ILink.cs
@@ -56,45 +56,4 @@ namespace Mutagen.Bethesda
         /// <returns>TryGet object with located record if successful</returns>
         ITryGetter<TMajor> TryResolve(ILinkCache package);
     }
-
-    /// <summary>
-    /// A static class with extension methods for ILink interfaces
-    /// </summary>
-    public static class ILinkExt
-    {
-        /// <summary>
-        /// Attempts to locate link target in given Link Cache.
-        /// </summary>
-        /// <param name="link">Link to resolve</param>
-        /// <param name="package">Link Cache to resolve against</param>
-        /// <param name="majorRecord">Major Record if located</param>
-        /// <returns>True if successful in linking to record</returns>
-        /// <typeparam name="TMajor">Major Record type to resolve to</typeparam>
-        public static bool TryResolve<TMajor>(this ILink<TMajor> link, ILinkCache package, [MaybeNullWhen(false)] out TMajor majorRecord)
-            where TMajor : IMajorRecordCommonGetter
-        {
-            var ret = link.TryResolve(package);
-            if (ret.Succeeded)
-            {
-                majorRecord = ret.Value;
-                return true;
-            }
-            majorRecord = default;
-            return false;
-        }
-
-        /// <summary>
-        /// Locates link target in given Link Cache.
-        /// </summary>
-        /// <param name="link">Link to resolve</param>
-        /// <param name="package">Link Cache to resolve against</param>
-        /// <returns>Located Major Record</returns>
-        /// <exception cref="NullReferenceException">If link was not succesful</exception>
-        /// <typeparam name="TMajor">Major Record type to resolve to</typeparam>
-        public static TMajor Resolve<TMajor>(this ILink<TMajor> link, ILinkCache package)
-            where TMajor : IMajorRecordCommonGetter
-        {
-            return link.TryResolve(package).Value;
-        }
-    }
 }
