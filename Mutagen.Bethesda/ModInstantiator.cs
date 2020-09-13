6d191040748ac41e75f9efdff8d347788dfb3937
Justin Swanson <justin.c.swanson@gmail.com>
2020-09-13 01:28:58 -0500

    Non generic ModInstantiator

---
+++ Mutagen.Bethesda/ModInstantiator.cs
@@ -0,0 +1,16 @@
+﻿using Mutagen.Bethesda.Internals;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mutagen.Bethesda
+{
+    public static class ModInstantiator
+    {
+        public static IModGetter Importer(ModPath path, GameRelease release)
+        {
+            var regis = release.ToCategory().ToModRegistration();
+            return ModInstantiatorReflection.GetOverlay(regis)(path, release);
+        }
+    }
+}
