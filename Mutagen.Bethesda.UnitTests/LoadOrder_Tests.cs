c1c71f50861859e8944a7a77649a8f0b4947a109
Justin Swanson <justin.c.swanson@gmail.com>
2020-09-24 19:29:51 -0500

    LoadOrder.GetLiveLoadOrder

--- Mutagen.Bethesda.UnitTests/LoadOrder_Tests.cs
+++ Mutagen.Bethesda.UnitTests/LoadOrder_Tests.cs
@@ -1,10 +1,11 @@
-﻿using Alphaleonis.Win32.Filesystem;
-using Iced.Intel;
+using Alphaleonis.Win32.Filesystem;
+using DynamicData;
 using Noggog.Utility;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 using Xunit;
 
 namespace Mutagen.Bethesda.UnitTests
@@ -125,7 +126,7 @@ namespace Mutagen.Bethesda.UnitTests
         [Fact]
         public void WriteImplicit()
         {
-            using var tmpFolder = new TempFolder(Path.Combine(Utility.TempFolderPath, nameof(WriteMarkers)));
+            using var tmpFolder = new TempFolder(Path.Combine(Utility.TempFolderPath, nameof(WriteImplicit)));
             var path = Path.Combine(tmpFolder.Dir.Path, "Plugins.txt");
             LoadOrder.Write(
                 path,
@@ -140,6 +141,48 @@ namespace Mutagen.Bethesda.UnitTests
             Assert.Equal(2, lines.Count);
             Assert.Equal($"*{Utility.ModKey2.FileName}", lines[0]);
             Assert.Equal($"{Utility.Dawnguard.FileName}", lines[1]);
+        }
+
+        [Fact]
+        public async Task LiveLoadOrder()
+        {
+            using var tmpFolder = new TempFolder(Path.Combine(Utility.TempFolderPath, nameof(LiveLoadOrder)));
+            var path = Path.Combine(tmpFolder.Dir.Path, "Plugins.txt");
+            File.WriteAllLines(path,
+                new string[]
+                {
+                    Skyrim.Constants.Skyrim.ToString(),
+                    Skyrim.Constants.Update.ToString(),
+                    Skyrim.Constants.Dawnguard.ToString(),
+                });
+            var live = LoadOrder.GetLiveLoadOrder(GameRelease.SkyrimLE, path, default, out var state);
+            var list = live.AsObservableList();
+            Assert.Equal(3, list.Count);
+            Assert.Equal(Skyrim.Constants.Skyrim, list.Items.ElementAt(0).ModKey);
+            Assert.Equal(Skyrim.Constants.Update, list.Items.ElementAt(1).ModKey);
+            Assert.Equal(Skyrim.Constants.Dawnguard, list.Items.ElementAt(2).ModKey);
+            File.WriteAllLines(path,
+                new string[]
+                {
+                    Skyrim.Constants.Skyrim.ToString(),
+                    Skyrim.Constants.Dawnguard.ToString(),
+                });
+            await Task.Delay(200);
+            Assert.Equal(2, list.Count);
+            Assert.Equal(Skyrim.Constants.Skyrim, list.Items.ElementAt(0).ModKey);
+            Assert.Equal(Skyrim.Constants.Dawnguard, list.Items.ElementAt(1).ModKey);
+            File.WriteAllLines(path,
+                new string[]
+                {
+                    Skyrim.Constants.Skyrim.ToString(),
+                    Skyrim.Constants.Dawnguard.ToString(),
+                    Skyrim.Constants.Dragonborn.ToString(),
+                });
+            await Task.Delay(200);
+            Assert.Equal(3, list.Count);
+            Assert.Equal(Skyrim.Constants.Skyrim, list.Items.ElementAt(0).ModKey);
+            Assert.Equal(Skyrim.Constants.Dawnguard, list.Items.ElementAt(1).ModKey);
+            Assert.Equal(Skyrim.Constants.Dragonborn, list.Items.ElementAt(2).ModKey);
         }
     }
 }
