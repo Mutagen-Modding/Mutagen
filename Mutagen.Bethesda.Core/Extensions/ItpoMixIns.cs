24f6b80320c6d1e3930c9e46fbfbdc600ce647c9
Justin Swanson <justin.c.swanson@gmail.com>
2020-10-18 22:50:13 -0500

    Some ITPO initial foundations

---
+++ Mutagen.Bethesda.Core/Extensions/ItpoMixIns.cs
@@ -0,0 +1,30 @@
+﻿using Noggog;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mutagen.Bethesda
+{
+    public static class ItpoMixIns
+    {
+        public static void RemoveIdentical<TMod>(this TMod mod, ILinkCache linkCache)
+            where TMod : class, IMod
+        {
+            var toRemove = new Dictionary<Type, HashSet<FormKey>>();
+            foreach (var major in mod.EnumerateMajorRecords())
+            {
+                var form = major.FormKey;
+                var type = major.GetType();
+                if (!linkCache.TryLookup(form, type, out var prevRec)) continue;
+                if (prevRec.Equals(major))
+                {
+                    toRemove.GetOrAdd(type).Add(form);
+                }
+            }
+            foreach (var type in toRemove)
+            {
+                mod.Remove(type.Value, type.Key);
+            }
+        }
+    }
+}
---
+++ Mutagen.Bethesda.UnitTests/IdenticalRemoval_Tests.cs
@@ -0,0 +1,30 @@
+﻿using Mutagen.Bethesda.Oblivion;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Mutagen.Bethesda.UnitTests
+{
+    public class IdenticalRemoval_Tests
+    {
+        [Fact]
+        public void NoRemovals()
+        {
+            var mod = new OblivionMod(Utility.ModKey);
+            var npc = mod.Npcs.AddNew();
+            var mod2 = new OblivionMod(Utility.ModKey2);
+            mod.RemoveIdentical(mod2.ToImmutableLinkCache());
+            Assert.Single(mod.Npcs);
+        }
+
+        [Fact]
+        public void SelfRemoval()
+        {
+            var mod = new OblivionMod(Utility.ModKey);
+            var npc = mod.Npcs.AddNew();
+            mod.RemoveIdentical(mod.ToImmutableLinkCache());
+            Assert.Empty(mod.Npcs);
+        }
+    }
+}
