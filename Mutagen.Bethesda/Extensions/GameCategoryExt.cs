3add4feb9a18afc86c20fc9f897e18e119174a77
Justin Swanson <justin.c.swanson@gmail.com>
2020-10-29 20:33:16 -0500

    GameCategory.DefaultRelease

--- Mutagen.Bethesda/Extensions/GameCategoryExt.cs
+++ Mutagen.Bethesda/Extensions/GameCategoryExt.cs
@@ -1,4 +1,4 @@
-﻿using Loqui;
+using Loqui;
 using Mutagen.Bethesda.Oblivion.Internals;
 using Mutagen.Bethesda.Skyrim.Internals;
 using System;
@@ -15,6 +15,16 @@ namespace Mutagen.Bethesda
             {
                 GameCategory.Oblivion => OblivionMod_Registration.Instance,
                 GameCategory.Skyrim => SkyrimMod_Registration.Instance,
+                _ => throw new NotImplementedException(),
+            };
+        }
+
+        public static GameRelease DefaultRelease(this GameCategory gameCategory)
+        {
+            return gameCategory switch
+            {
+                GameCategory.Oblivion => GameRelease.Oblivion,
+                GameCategory.Skyrim => GameRelease.SkyrimSE,
                 _ => throw new NotImplementedException(),
             };
         }
