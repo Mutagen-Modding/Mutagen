66562f7b2405503ec7e0599629f0f634ddb6a166
Deigue <deigue@hotmail.com>
2020-11-17 20:35:16 -0500

    API enhancement: Disable() for IPlaced entities.

---
+++ Mutagen.Bethesda.Skyrim/Interfaces/IPlaced.cs
@@ -0,0 +1,29 @@
+﻿using Noggog;
+
+namespace Mutagen.Bethesda.Skyrim
+{
+    public partial interface IPlaced
+    {
+        public Placement? Placement { get; set; }
+        
+        public EnableParent? EnableParent { get; set; }
+        
+        public int MajorRecordFlagsRaw { get; set; }
+
+        public bool Disable()
+        {
+            // Perform standard procedure used by xEdit for undelete and disable records.
+            if (this.IsDeleted) return false;
+
+            if (Placement != null) Placement.Position = new P3Float(Placement.Position.X, Placement.Position.Y, -30000);
+            if (EnableParent != null)
+            {
+                EnableParent.Flags = EnableParent.Flag.SetEnableStateToOppositeOfParent;
+                EnableParent.Reference = new FormLink<ILinkedReferenceGetter>(Constants.Player);
+            }
+
+            MajorRecordFlagsRaw = (int) SkyrimMajorRecord.SkyrimMajorRecordFlag.InitiallyDisabled;
+            return true;
+        }
+    }
+}
