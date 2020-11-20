af04ba3199c6f2c3e552d554bbce852f8afd9e5d
Deigue <deigue@hotmail.com>
2020-11-20 12:38:01 -0500

    Removed OverrideAndDisable extension APIs

--- Mutagen.Bethesda.Core/Extensions/OverrideMixIns.cs
+++ Mutagen.Bethesda.Core/Extensions/OverrideMixIns.cs
@@ -456,23 +456,5 @@ namespace Mutagen.Bethesda
                 throw RecordException.Factory(ex, link.FormKey, edid: null);
             }
         }
-
-        /// <summary>
-        /// Takes in an existing record definition, and either finds the existing override definition
-        /// from the Group, or copies the given record and inserts it. This overriden record is disabled if
-        /// possible and returned to the user if any further operations are required.
-        /// </summary>
-        /// <param name="group">Group to retrieve and/or insert from</param>
-        /// <param name="major">Major record to query and potentially copy</param>
-        /// <returns>Corresponding record that has been overridden and disabled in the provided group.</returns>
-        public static TMajor OverrideAndDisable<TMajor, TMajorGetter>(this IGroupCommon<TMajor> group,
-            TMajorGetter major)
-            where TMajor : class, IMajorRecordInternal, TMajorGetter
-            where TMajorGetter : class, IMajorRecordGetter, IBinaryItem
-        {
-            var overridenMajor = GetOrAddAsOverride(group, major);
-            overridenMajor.Disable();
-            return overridenMajor;
-        }
     }
 }
