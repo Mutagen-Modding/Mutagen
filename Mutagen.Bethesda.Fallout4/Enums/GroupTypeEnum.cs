41fb26d8f052d2a5c9d11f40940933db61d3308e
Justin Swanson <justin.c.swanson@gmail.com>
2021-01-18 19:53:49 -0600

    Removed GroupTypeEnum from core

---
+++ Mutagen.Bethesda.Fallout4/Enums/GroupTypeEnum.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mutagen.Bethesda.Fallout4
+{
+    /// <summary>
+    /// Different categories of Group records
+    /// </summary>
+    public enum GroupTypeEnum
+    {
+        Type = 0,
+        WorldChildren = 1,
+        InteriorCellBlock = 2,
+        InteriorCellSubBlock = 3,
+        ExteriorCellBlock = 4,
+        ExteriorCellSubBlock = 5,
+        CellChildren = 6,
+        TopicChildren = 7,
+        CellPersistentChildren = 8,
+        CellTemporaryChildren = 9
+    }
+}
