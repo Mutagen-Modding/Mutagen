c44877770682952cc6f6a749f4752b05f78708e9
Justin Swanson <justin.c.swanson@gmail.com>
2021-03-01 17:03:49 -0600

    AlchemicalApparatus.QualityLevel migrated to common enum

---
+++ Mutagen.Bethesda.Skyrim/Enums/QualityLevel.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mutagen.Bethesda.Skyrim
+{
+    // Alchemical apparatus uses these numeric assignments for parsing
+
+    public enum QualityLevel
+    {
+        Novice = 0,
+        Apprentice = 1,
+        Journeyman = 2,
+        Expert = 3,
+        Master = 4
+    }
+}
