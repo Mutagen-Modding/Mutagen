fac29cc62cbb907679141e52b2903c15f051cf99
Justin Swanson <justin.c.swanson@gmail.com>
2021-03-20 00:33:18 -0500

    Removed FormLink implicit operator to FormKey
    Was causing confusion when passing to functions that could take links or formkeys

--- Mutagen.Bethesda.Core/Links/FormLink.cs
+++ Mutagen.Bethesda.Core/Links/FormLink.cs
@@ -222,11 +222,6 @@ namespace Mutagen.Bethesda
         {
             return new FormLink<TMajorGetter>(formKey);
         }
-
-        public static implicit operator FormKey(FormLink<TMajorGetter> link)
-        {
-            return link.FormKey;
-        }
     }
 
     public struct FormLink<TMajor, TMajorGetter>
