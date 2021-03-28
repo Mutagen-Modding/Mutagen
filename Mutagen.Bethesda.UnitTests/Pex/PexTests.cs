ebec32f012b7f58d8b20a59ca3a3880cd0c3c362
Justin Swanson <justin.c.swanson@gmail.com>
2021-03-28 18:23:20 -0500

    Added failing test for additions
    Since string table is stored after parsing is finished, it still can be put out of sync, as shown by this test

--- Mutagen.Bethesda.UnitTests/Pex/PexTests.cs
+++ Mutagen.Bethesda.UnitTests/Pex/PexTests.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Security.Cryptography;
+using FluentAssertions;
 using Mutagen.Bethesda.Pex;
 using Xunit;
 
@@ -29,8 +30,8 @@ namespace Mutagen.Bethesda.UnitTests.Pex
             //from https://www.nexusmods.com/skyrimspecialedition/mods/18076
             new object[]{ "nwsFollowerMCMExScript.pex", GameCategory.Skyrim },
             new object[]{ "nwsFollowerMCMScript.pex", GameCategory.Skyrim },
-    };
-        
+        };
+
         [Theory]
         [MemberData(nameof(TestDataFiles))]
         public void TestPexParsing(string file, GameCategory gameCategory)
@@ -57,15 +58,15 @@ namespace Mutagen.Bethesda.UnitTests.Pex
 
             var outputPex = PexFile.CreateFromFile(outputFile, gameCategory);
             Assert.NotNull(outputPex);
-            
+
             var inputFi = new FileInfo(inputFile);
             var outputFi = new FileInfo(outputFile);
-            
+
             Assert.Equal(inputFi.Length, outputFi.Length);
 
             var inputHash = SHA256.HashData(File.ReadAllBytes(inputFile));
             var outputHash = SHA256.HashData(File.ReadAllBytes(outputFile));
-            
+
             Assert.Equal(inputHash, outputHash);
         }
 
@@ -76,12 +77,12 @@ namespace Mutagen.Bethesda.UnitTests.Pex
             Assert.True(File.Exists(path));
 
             var pex = PexFile.CreateFromFile(path, GameCategory.Skyrim);
-            
+
             Assert.Equal(0xFA57C0DE, pex.Magic);
             Assert.Equal(3, pex.MajorVersion);
             Assert.Equal(2, pex.MinorVersion);
             Assert.Equal(1, pex.GameId);
-            Assert.Equal(((ulong) 0x5F21B0ED).ToDateTime(), pex.CompilationTime);
+            Assert.Equal(((ulong)0x5F21B0ED).ToDateTime(), pex.CompilationTime);
             Assert.Equal("Art.psc", pex.SourceFileName);
             Assert.Equal(string.Empty, pex.Username);
             Assert.Equal(string.Empty, pex.MachineName);
@@ -98,12 +99,12 @@ namespace Mutagen.Bethesda.UnitTests.Pex
             Assert.Single(objects);
 
             var mainObject = objects.First();
-            
+
             Assert.Equal("Art", mainObject.Name);
             Assert.Equal("Form", mainObject.ParentClassName);
             Assert.Equal(string.Empty, mainObject.DocString);
             Assert.Equal(string.Empty, mainObject.AutoStateName);
-            
+
             Assert.Empty(mainObject.Properties);
             Assert.Empty(mainObject.Variables);
             Assert.Single(mainObject.States);
@@ -111,6 +112,30 @@ namespace Mutagen.Bethesda.UnitTests.Pex
             var state = mainObject.States.First();
             Assert.Equal(string.Empty, state.Name);
             Assert.Equal(4, state.Functions.Count);
+        }
+
+        [Fact]
+        public void TestPexAddition()
+        {
+            var path = Path.Combine("Pex", "files", "Art.pex");
+            Assert.True(File.Exists(path));
+
+            var pex = PexFile.CreateFromFile(path, GameCategory.Skyrim);
+            var functionToAdd = new DebugFunction(pex)
+            {
+                FunctionName = "HelloWorld",
+                FunctionType = DebugFunctionType.Method,
+            };
+            pex.DebugInfo?.Functions.Add(functionToAdd);
+
+            using var tempFolder = Utility.GetTempFolder(nameof(PexTests));
+            var outPath = Path.Combine(tempFolder.Dir.Path, Path.GetTempFileName());
+            pex.WritePexFile(outPath, GameCategory.Skyrim);
+
+            var pex2 = PexFile.CreateFromFile(outPath, GameCategory.Skyrim);
+            pex2.DebugInfo.Should().NotBeNull();
+            pex2.DebugInfo!.Functions.Should().HaveCount(pex.DebugInfo!.Functions.Count);
+            pex2.DebugInfo!.Functions[^1].FunctionName.Should().Be(functionToAdd.FunctionName);
         }
     }
 }
