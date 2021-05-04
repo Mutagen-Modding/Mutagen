677712abdba267f612b2c262d473b14e429e7b3f
Justin Swanson <justin.c.swanson@gmail.com>
2021-05-03 19:02:01 -0500

    Mutagen.Bethesda proj cleaning

--- Mutagen.Bethesda/Plugins/Records/Internals/GameCategoryExt.cs
+++ Mutagen.Bethesda/Plugins/Records/Internals/GameCategoryExt.cs
@@ -3,10 +3,8 @@ using Mutagen.Bethesda.Oblivion.Internals;
 using Mutagen.Bethesda.Skyrim.Internals;
 using Mutagen.Bethesda.Fallout4.Internals;
 using System;
-using System.Collections.Generic;
-using System.Text;
 
-namespace Mutagen.Bethesda
+namespace Mutagen.Bethesda.Plugins.Records.Internals
 {
     public static class GameCategoryExt
     {
@@ -16,7 +14,7 @@ namespace Mutagen.Bethesda
             {
                 GameCategory.Oblivion => OblivionMod_Registration.Instance,
                 GameCategory.Skyrim => SkyrimMod_Registration.Instance,
-                GameCategory.Fallout4=> Fallout4Mod_Registration.Instance,
+                GameCategory.Fallout4 => Fallout4Mod_Registration.Instance,
                 _ => throw new NotImplementedException(),
             };
         }
