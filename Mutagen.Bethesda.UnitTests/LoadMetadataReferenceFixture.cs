f518d5933e573e9640af7b8f5de51fee488b8e70
Justin Swanson <justin.c.swanson@gmail.com>
2021-06-15 21:10:43 -0500

    Initial custom aspect source generator

---
+++ Mutagen.Bethesda.UnitTests/LoadMetadataReferenceFixture.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using Microsoft.CodeAnalysis;
+using Noggog;
+
+namespace Mutagen.Bethesda.UnitTests
+{
+    public class LoadMetadataReferenceFixture
+    {
+        public List<MetadataReference> MetadataReferences { get; } = new List<MetadataReference>();
+
+        public bool LoadMutagen = true;
+        
+        public LoadMetadataReferenceFixture()
+        {
+            Assembly[] assemblies = AppDomain.CurrentDomain.GetAssemblies();
+            foreach (var assembly in assemblies)
+            {
+                if (assembly.IsDynamic) continue;
+                if (Want(assembly))
+                {
+                    MetadataReferences.Add(MetadataReference.CreateFromFile(assembly.Location));
+                }
+            }
+        }
+
+        public bool Want(Assembly assembly)
+        {
+            var fileName = Path.GetFileName(assembly.Location);
+            if (LoadMutagen
+                && fileName.Contains("Mutagen")
+                && fileName != "Mutagen.Bethesda.UnitTests.dll")
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
