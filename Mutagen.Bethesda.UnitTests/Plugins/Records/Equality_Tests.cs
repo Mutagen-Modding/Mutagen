1aedd8fe3f98d4104d3c984536b6176ee85cc62f
Justin Swanson <justin.c.swanson@gmail.com>
2021-06-21 19:36:40 -0500

    Failing equality test

---
+++ Mutagen.Bethesda.UnitTests/Plugins/Records/Equality_Tests.cs
@@ -0,0 +1,75 @@
+﻿using FluentAssertions;
+using Mutagen.Bethesda.Plugins;
+using Mutagen.Bethesda.Skyrim;
+using Xunit;
+
+namespace Mutagen.Bethesda.UnitTests.Plugins.Records
+{
+    public class Equality_Tests
+    {
+        private Weapon GetSomeWeapon()
+        {
+            return new Weapon(FormKey.Null, SkyrimRelease.SkyrimSE)
+            {
+                Description = "Test",
+                BasicStats = new WeaponBasicStats()
+                {
+                    Damage = 5
+                },
+                Unused = new byte[] {1, 2, 3},
+                AttackSound = Utility.Form2.AsNullableLink<ISoundDescriptorGetter>()
+            };
+        }
+        
+        [Fact]
+        public void WeaponBasicStats()
+        {
+            Weapon.TranslationMask _verificationMask = new(defaultOn: true)
+            {
+                BasicStats = new WeaponBasicStats.TranslationMask(defaultOn: true)
+                {
+                    Damage = false,
+                    Value = false,
+                    Weight = false,
+                }
+            };
+
+            var someWeapon = GetSomeWeapon();
+
+            var other = someWeapon.DeepCopy();
+            other.BasicStats!.Damage = 10;
+
+            someWeapon.Equals(other, _verificationMask)
+                .Should().BeTrue();
+        }
+        
+        [Fact]
+        public void SelfEquality()
+        {
+            var someWeapon = GetSomeWeapon();
+            someWeapon.Equals(someWeapon)
+                .Should().BeTrue();
+        }
+        
+        [Fact]
+        public void DuplicatedEquality()
+        {
+            var someWeapon = GetSomeWeapon();
+            var other = someWeapon.DeepCopy();
+
+            someWeapon.Equals(other)
+                .Should().BeTrue();
+        }
+        
+        [Fact]
+        public void DuplicatedInequality()
+        {
+            var someWeapon = GetSomeWeapon();
+            var other = someWeapon.DeepCopy();
+            other.BasicStats!.Damage = 10;
+
+            someWeapon.Equals(other)
+                .Should().BeFalse();
+        }
+    }
+}
