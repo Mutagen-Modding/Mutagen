933f81577a04f1361f4d1a40dc0678c3baa1c8e1
Justin Swanson <justin.c.swanson@gmail.com>
2021-06-24 00:11:04 -0500

    Mutagen.Bethesda.Core.Windows root namespace switched

--- Mutagen.Bethesda.Core.Windows/Installs/GameLocations.cs
+++ Mutagen.Bethesda.Core.Windows/Installs/GameLocations.cs
@@ -18,38 +18,38 @@ namespace Mutagen.Bethesda.Installs
     {
         private static readonly GameLocator Locator = new();
         
-        /// <inheritdoc cref="IGameLocator" />
+        /// <inheritdoc cref="GameLocator" />
         public static IEnumerable<DirectoryPath> GetGameFolders(GameRelease release)
         {
             return Locator.GetGameFolders(release);
         }
 
-        /// <inheritdoc cref="IGameLocator" />
+        /// <inheritdoc cref="GameLocator" />
         public static bool TryGetGameFolderFromRegistry(GameRelease release,
             [MaybeNullWhen(false)] out DirectoryPath path)
         {
             return Locator.TryGetGameFolderFromRegistry(release, out path);
         }
         
-        /// <inheritdoc cref="IGameLocator" />
+        /// <inheritdoc cref="GameLocator" />
         public static bool TryGetGameFolder(GameRelease release, [MaybeNullWhen(false)] out DirectoryPath path)
         {
             return Locator.TryGetGameFolder(release, out path);
         }
 
-        /// <inheritdoc cref="IGameLocator" />
+        /// <inheritdoc cref="GameLocator" />
         public static DirectoryPath GetGameFolder(GameRelease release)
         {
             return Locator.GetGameFolder(release);
         }
 
-        /// <inheritdoc cref="IGameLocator" />
+        /// <inheritdoc cref="GameLocator" />
         public static bool TryGetDataFolder(GameRelease release, [MaybeNullWhen(false)] out DirectoryPath path)
         {
             return Locator.TryGetDataFolder(release, out path);
         }
 
-        /// <inheritdoc cref="IGameLocator" />
+        /// <inheritdoc cref="GameLocator" />
         public static DirectoryPath GetDataFolder(GameRelease release)
         {
             return Locator.GetDataFolder(release);
