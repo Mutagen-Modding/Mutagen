e2dc7d7a629047e3d1bc8030c127c6338a34ce4e
Justin Swanson <justin.c.swanson@gmail.com>
2021-07-04 16:05:10 -0500

    Readded GameLocator comments

--- Mutagen.Bethesda.Core.Windows/Installs/DI/GameLocator.cs
+++ Mutagen.Bethesda.Core.Windows/Installs/DI/GameLocator.cs
@@ -23,14 +23,22 @@ namespace Mutagen.Bethesda.Installs.DI
             _gogHandler = new(TryFactory<GOGHandler, GOGGame>);
         }
 
-        /// <inheritdoc />
+        /// <summary>
+        /// Given a release, will return all the located game folders it could find
+        /// </summary>
+        /// <param name="release">Release to query</param>
+        /// <returns>The located game folders it could find</returns>
         public IEnumerable<DirectoryPath> GetGameDirectories(GameRelease release)
         {
             return InternalGetGameFolders(release)
                 .Distinct();
         }
 
-        /// <inheritdoc />
+        /// <summary>
+        /// Given a release, will return all the located data folders it could find
+        /// </summary>
+        /// <param name="release">Release to query</param>
+        /// <returns>The located data folders it could find</returns>
         public IEnumerable<DirectoryPath> GetDataDirectories(GameRelease release)
         {
             return GetGameDirectories(release)
@@ -64,7 +72,6 @@ namespace Mutagen.Bethesda.Installs.DI
             }
         }
         
-        /// <inheritdoc />
         public bool TryGetGameDirectoryFromRegistry(GameRelease release,
             [MaybeNullWhen(false)] out DirectoryPath path)
         {
@@ -95,7 +102,12 @@ namespace Mutagen.Bethesda.Installs.DI
             }
         }
         
-        /// <inheritdoc />
+        /// <summary>
+        /// Given a release, tries to retrieve the preferred game directory (not the data folder within)
+        /// </summary>
+        /// <param name="release">Release to query</param>
+        /// <param name="path">The game folder, if located</param>
+        /// <returns>True if located</returns>
         public bool TryGetGameDirectory(GameRelease release, [MaybeNullWhen(false)] out DirectoryPath path)
         {
             var p = GetGameDirectories(release).Select<DirectoryPath, DirectoryPath?>(x => x).FirstOrDefault();
@@ -109,7 +121,12 @@ namespace Mutagen.Bethesda.Installs.DI
             return true;
         }
 
-        /// <inheritdoc />
+        /// <summary>
+        /// Given a release, tries to retrieve the preferred game directory (not the data folder within)
+        /// </summary>
+        /// <param name="release">Release to query</param>
+        /// <exception cref="System.IO.DirectoryNotFoundException">Thrown if the game folder could not be located</exception>
+        /// <returns>The game folder</returns>
         public DirectoryPath GetGameDirectory(GameRelease release)
         {
             if (TryGetGameDirectory(release, out var path))
@@ -119,7 +136,12 @@ namespace Mutagen.Bethesda.Installs.DI
             throw new DirectoryNotFoundException($"Game folder for {release} cannot be found automatically");
         }
 
-        /// <inheritdoc />
+        /// <summary>
+        /// Given a release, tries to retrieve the preferred data directory
+        /// </summary>
+        /// <param name="release">Release to query</param>
+        /// <param name="path">The data folder, if located</param>
+        /// <returns>True if located</returns>
         public bool TryGetDataDirectory(GameRelease release, [MaybeNullWhen(false)] out DirectoryPath path)
         {
             if (TryGetGameDirectory(release, out path))
@@ -131,7 +153,12 @@ namespace Mutagen.Bethesda.Installs.DI
             return false;
         }
 
-        /// <inheritdoc />
+        /// <summary>
+        /// Given a release, tries to retrieve the preferred data directory
+        /// </summary>
+        /// <param name="release">Release to query</param>
+        /// <exception cref="System.IO.DirectoryNotFoundException">Thrown if the data folder could not be located</exception>
+        /// <returns>The data folder provider</returns>
         public DirectoryPath GetDataDirectory(GameRelease release)
         {
             if (TryGetDataDirectory(release, out var path))
