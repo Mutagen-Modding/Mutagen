1148417aa528e60adeb130501964dc2c3f5425c6
Justin Swanson <justin.c.swanson@gmail.com>
2021-07-12 17:31:29 -0500

    MutagenAutoDataCustomization houses more logic

--- Mutagen.Bethesda.UnitTests/AutoData/MutagenAutoData.cs
+++ Mutagen.Bethesda.UnitTests/AutoData/MutagenAutoData.cs
@@ -1,28 +1,21 @@
 ﻿using AutoFixture;
-using AutoFixture.AutoFakeItEasy;
 using AutoFixture.Xunit2;
-using Noggog.Testing.AutoFixture;
 
 namespace Mutagen.Bethesda.UnitTests.AutoData
 {
     public class MutagenAutoDataAttribute : AutoDataAttribute
     {
-        public MutagenAutoDataAttribute(bool Strict = true, bool ConfigureMembers = false, GameRelease Release = GameRelease.SkyrimSE)
+        public MutagenAutoDataAttribute(
+            bool Strict = true,
+            bool ConfigureMembers = false, 
+            GameRelease Release = GameRelease.SkyrimSE)
             : base(() =>
             {
-                var customization = new AutoFakeItEasyCustomization()
-                {
-                    ConfigureMembers = ConfigureMembers,
-                    GenerateDelegates = true
-                };
-                if (Strict)
-                {
-                    customization.Relay = new FakeItEasyStrictRelay();
-                }
                 return new Fixture()
-                    .Customize(customization)
-                    .Customize(new MutagenAutoDataCustomization(Release))
-                    .Customize(new FileSystemCustomization());
+                    .Customize(new MutagenAutoDataCustomization(
+                        strict: Strict,
+                        configureMembers: ConfigureMembers,
+                        release: Release));
             })
         {
         }
--- Mutagen.Bethesda.UnitTests/AutoData/MutagenAutoDataCustomization.cs
+++ Mutagen.Bethesda.UnitTests/AutoData/MutagenAutoDataCustomization.cs
@@ -1,19 +1,39 @@
 ﻿using AutoFixture;
+using AutoFixture.AutoFakeItEasy;
+using Noggog.Testing.AutoFixture;
 
 namespace Mutagen.Bethesda.UnitTests.AutoData
 {
     public class MutagenAutoDataCustomization : ICustomization
     {
         private readonly GameRelease _release;
+        private readonly bool _strict;
+        private readonly bool _configureMembers;
 
-        public MutagenAutoDataCustomization(GameRelease release)
+        public MutagenAutoDataCustomization(
+            bool strict,
+            bool configureMembers, 
+            GameRelease release)
         {
             _release = release;
+            _strict = strict;
+            _configureMembers = configureMembers;
         }
         
         public void Customize(IFixture fixture)
         {
+            var autoFakeItEasy = new AutoFakeItEasyCustomization()
+            {
+                ConfigureMembers = _configureMembers,
+                GenerateDelegates = true
+            };
+            if (_strict)
+            {
+                autoFakeItEasy.Relay = new FakeItEasyStrictRelay();
+            }
+            fixture.Customize(autoFakeItEasy);
             fixture.Customizations.Add(new MutagenSpecimenBuilder(_release));
+            fixture.Customize(new FileSystemCustomization());
         }
     }
 }
