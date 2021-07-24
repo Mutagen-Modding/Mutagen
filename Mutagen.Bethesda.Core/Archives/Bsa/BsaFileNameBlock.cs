d8702dc7b2edaee40120bb4c93d58e1267baa734
Justin Swanson <justin.c.swanson@gmail.com>
2021-07-24 12:05:00 -0500

    Enrich BsaFileNameBlock exceptions

--- Mutagen.Bethesda.Core/Archives/Bsa/BsaFileNameBlock.cs
+++ Mutagen.Bethesda.Core/Archives/Bsa/BsaFileNameBlock.cs
@@ -1,6 +1,7 @@
 using Noggog;
 using System;
 using System.IO;
+using Mutagen.Bethesda.Archives.Exceptions;
 
 namespace Mutagen.Bethesda.Archives.Bsa
 {
@@ -14,24 +15,33 @@ namespace Mutagen.Bethesda.Archives.Bsa
                 mode: System.Threading.LazyThreadSafetyMode.ExecutionAndPublication,
                 valueFactory: () =>
                 {
-                    using var stream = bsa.GetStream();
-                    stream.BaseStream.Position = position;
-                    ReadOnlyMemorySlice<byte> data = stream.ReadBytes(checked((int)bsa.TotalFileNameLength));
-                    ReadOnlyMemorySlice<byte>[] names = new ReadOnlyMemorySlice<byte>[bsa.FileCount];
-                    for (int i = 0; i < bsa.FileCount; i++)
+                    try
                     {
-                        var index = data.Span.IndexOf(default(byte));
-                        if (index == -1)
+                        using var stream = bsa.GetStream();
+                        stream.BaseStream.Position = position;
+                        ReadOnlyMemorySlice<byte> data = stream.ReadBytes(checked((int)bsa.TotalFileNameLength));
+                        ReadOnlyMemorySlice<byte>[] names = new ReadOnlyMemorySlice<byte>[bsa.FileCount];
+                        for (int i = 0; i < bsa.FileCount; i++)
                         {
-                            throw new InvalidDataException("Did not end all of its strings in null bytes");
+                            var index = data.Span.IndexOf(default(byte));
+                            if (index == -1)
+                            {
+                                throw new InvalidDataException("Did not end all of its strings in null bytes");
+                            }
+                            names[i] = data.Slice(0, index + 1);
+                            var str = names[i].ReadStringTerm(bsa.HeaderType);
+                            data = data.Slice(index + 1);
                         }
-                        names[i] = data.Slice(0, index + 1);
-                        var str = names[i].ReadStringTerm(bsa.HeaderType);
-                        data = data.Slice(index + 1);
+                        // Data doesn't seem to need to be fully consumed.
+                        // Official BSAs have overflow of zeros
+                        return names;
                     }
-                    // Data doesn't seem to need to be fully consumed.
-                    // Official BSAs have overflow of zeros
-                    return names;
+                    catch (Exception e)
+                    when (bsa.FilePath != null)
+                    {
+                        throw ArchiveException.EnrichWithArchivePath("Strings section was not able to be read", e,
+                            bsa.FilePath.Value);
+                    }
                 });
         }
     }
