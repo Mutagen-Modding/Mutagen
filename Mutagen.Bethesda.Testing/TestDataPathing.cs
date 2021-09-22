033b11efe7b41e691ef952acebfc1754e331e670
Justin Swanson <justin.c.swanson@gmail.com>
2021-09-22 18:59:20 -0500

    Test file location refactoring

--- Mutagen.Bethesda.Testing/TestDataPathing.cs
+++ Mutagen.Bethesda.Testing/TestDataPathing.cs
@@ -9,29 +9,32 @@ namespace Mutagen.Bethesda.Testing
 {
     public class TestDataPathing
     {
-        public static ModPath OblivionTestMod = new ModPath(ModKey.FromNameAndExtension("test.esp"), "Oblivion/oblivion_test.esp");
-        public static ModPath OblivionOverrideMod = new ModPath(ModKey.FromNameAndExtension("override.esp"), "Oblivion/oblivion_override.esp");
-        public static ModPath SkyrimTestMod = ModPath.FromPath("Skyrim/skyrim_test.esp");
-        public static ModPath SkyrimOverrideMod = ModPath.FromPath("Skyrim/skyrim_override.esp");
-        public static ModPath SizeOverflow = ModPath.FromPath("Skyrim/size_overflow_test.esp");
-        public static ModPath SubObjectSizeOverflow = ModPath.FromPath("Skyrim/subobject_size_overflow_test.esp");
-        public static ModPath RaceHeadPartDanglingMaster = ModPath.FromPath("Skyrim/RaceHeadPartDanglingMarker.esp");
-        public static ModPath SkyrimPerkFunctionParametersTypeNone = ModPath.FromPath("Skyrim/SkyrimPerkFunctionParametersTypeNone.esp");
-        public static string SkyrimBodtLength8With42 = "Skyrim/BodyTemplate/BodtLength8With42.esp";
-        public static string SkyrimBodtLength12With42 = "Skyrim/BodyTemplate/BodtLength12With42.esp";
-        public static string SkyrimBodtLength8With43 = "Skyrim/BodyTemplate/BodtLength8With43.esp";
-        public static string SkyrimBodtLength12With43 = "Skyrim/BodyTemplate/BodtLength12With43Normal.esp";
-        public static string SkyrimBodtLength8With44 = "Skyrim/BodyTemplate/BodtLength8With44.esp";
-        public static string SkyrimBodtLength12With44 = "Skyrim/BodyTemplate/BodtLength12With44.esp";
-        public static string SkyrimBod2Length8With42 = "Skyrim/BodyTemplate/Bod2Length8With42.esp";
-        public static string SkyrimBod2Length12With42 = "Skyrim/BodyTemplate/Bod2Length12With42.esp";
-        public static string SkyrimBod2Length12With43 = "Skyrim/BodyTemplate/Bod2Length12With43.esp";
-        public static string SkyrimBod2Length8With43 = "Skyrim/BodyTemplate/Bod2Length8With43.esp";
-        public static string SkyrimBod2Length8With44 = "Skyrim/BodyTemplate/Bod2Length8With44Normal.esp";
-        public static string SkyrimBod2Length12With44 = "Skyrim/BodyTemplate/Bod2Length12With44.esp";
-        public static string SkyrimBodtTypicalOutput = "Skyrim/BodyTemplate/BodtTypicalOutput";
-        public static string SkyrimBodtEmptyFlagsOutput = "Skyrim/BodyTemplate/BodtEmptyFlagsOutput";
-        public static string SkyrimBod2TypicalOutput = "Skyrim/BodyTemplate/Bod2TypicalOutput";
+        public static ModPath OblivionTestMod = new ModPath(ModKey.FromNameAndExtension("test.esp"), "Files/Oblivion/oblivion_test.esp");
+        public static ModPath OblivionOverrideMod = new ModPath(ModKey.FromNameAndExtension("override.esp"), "Files/Oblivion/oblivion_override.esp");
+        public static ModPath SkyrimTestMod = ModPath.FromPath("Files/Skyrim/skyrim_test.esp");
+        public static ModPath SkyrimOverrideMod = ModPath.FromPath("Files/Skyrim/skyrim_override.esp");
+        public static ModPath SizeOverflow = ModPath.FromPath("Files/Skyrim/size_overflow_test.esp");
+        public static ModPath SubObjectSizeOverflow = ModPath.FromPath("Files/Skyrim/subobject_size_overflow_test.esp");
+        public static ModPath RaceHeadPartDanglingMaster = ModPath.FromPath("Files/Skyrim/RaceHeadPartDanglingMarker.esp");
+        public static ModPath SkyrimPerkFunctionParametersTypeNone = ModPath.FromPath("Files/Skyrim/SkyrimPerkFunctionParametersTypeNone.esp");
+        public static string SkyrimBodtLength8With42 = "Files/Skyrim/BodyTemplate/BodtLength8With42.esp";
+        public static string SkyrimBodtLength12With42 = "Files/Skyrim/BodyTemplate/BodtLength12With42.esp";
+        public static string SkyrimBodtLength8With43 = "Files/Skyrim/BodyTemplate/BodtLength8With43.esp";
+        public static string SkyrimBodtLength12With43 = "Files/Skyrim/BodyTemplate/BodtLength12With43Normal.esp";
+        public static string SkyrimBodtLength8With44 = "Files/Skyrim/BodyTemplate/BodtLength8With44.esp";
+        public static string SkyrimBodtLength12With44 = "Files/Skyrim/BodyTemplate/BodtLength12With44.esp";
+        public static string SkyrimBod2Length8With42 = "Files/Skyrim/BodyTemplate/Bod2Length8With42.esp";
+        public static string SkyrimBod2Length12With42 = "Files/Skyrim/BodyTemplate/Bod2Length12With42.esp";
+        public static string SkyrimBod2Length12With43 = "Files/Skyrim/BodyTemplate/Bod2Length12With43.esp";
+        public static string SkyrimBod2Length8With43 = "Files/Skyrim/BodyTemplate/Bod2Length8With43.esp";
+        public static string SkyrimBod2Length8With44 = "Files/Skyrim/BodyTemplate/Bod2Length8With44Normal.esp";
+        public static string SkyrimBod2Length12With44 = "Files/Skyrim/BodyTemplate/Bod2Length12With44.esp";
+        public static string SkyrimBodtTypicalOutput = "Files/Skyrim/BodyTemplate/BodtTypicalOutput";
+        public static string SkyrimBodtEmptyFlagsOutput = "Files/Skyrim/BodyTemplate/BodtEmptyFlagsOutput";
+        public static string SkyrimBod2TypicalOutput = "Files/Skyrim/BodyTemplate/Bod2TypicalOutput";
+        public static string FrenchString = "Files/Core/Strings/FrenchString";
+        public static string PrependedString = "Files/Core/Strings/PrependedString";
+        public static string ZeroContentPrependedString = "Files/Core/Strings/ZeroContentPrependedString";
 
         public static MutagenFrame GetReadFrame(ModPath path, GameRelease release, ModKey? modKey = null)
         {
