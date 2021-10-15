0d1bbb882b41ceeb446bfc23e1ff854ae6e065af
Justin Swanson <justin.c.swanson@gmail.com>
2021-10-15 16:27:02 -0500

    Fix for RecordExceptions throwing if passed umbrella interfaces

---
+++ Mutagen.Bethesda.Core.UnitTests/Plugins/Exceptions/RecordExceptionTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using FluentAssertions;
+using Mutagen.Bethesda.Plugins;
+using Mutagen.Bethesda.Plugins.Exceptions;
+using Mutagen.Bethesda.Skyrim;
+using Xunit;
+
+namespace Mutagen.Bethesda.UnitTests.Plugins.Exceptions
+{
+    public class RecordExceptionTests
+    {
+        [Fact]
+        public void EnrichWithUmbrellaInterface()
+        {
+            var ex = new Exception();
+            var rec = RecordException.Enrich<IItem>(ex, FormKey.Null, "SomeEdid");
+            rec.RecordType.Should().Be(typeof(IItem));
+        }
+        
+        [Fact]
+        public void CreateWithUmbrellaInterface()
+        {
+            var ex = new Exception();
+            var rec = RecordException.Create<IItem>("Message", FormKey.Null, "SomeEdid");
+            rec.RecordType.Should().Be(typeof(IItem));
+        }
+    }
+}
