4fe577bc1e42efe551a1a0748f5dcecfb4089136
Justin Swanson <justin.c.swanson@gmail.com>
2021-12-11 20:49:48 -0600

    Disabled sql allocator tests

--- Mutagen.Bethesda.Core.UnitTests/Plugins/Allocators/SQLiteFormKeyAllocator_Tests.cs
+++ Mutagen.Bethesda.Core.UnitTests/Plugins/Allocators/SQLiteFormKeyAllocator_Tests.cs
@@ -8,48 +8,48 @@ using Noggog.Utility;
 
 namespace Mutagen.Bethesda.Core.UnitTests.Plugins.Allocators
 {
-    public class SQLiteFormKeyAllocator_Tests : ISharedFormKeyAllocator_Tests<SQLiteFormKeyAllocator>, IDisposable
-    {
-        protected override IFileSystem GetFileSystem() => IFileSystemExt.DefaultFilesystem;
-
-        protected Lazy<TempFolder> tempFolder;
-
-        protected Lazy<TempFile> tempFile;
-
-        private bool disposedValue;
-
-        public SQLiteFormKeyAllocator_Tests()
-        {
-            tempFolder = new(() => TempFolder.Factory());
-            tempFile = new(() => new TempFile(extraDirectoryPaths: TestPathing.TempFolderPath));
-        }
-        
-        protected override string ConstructTypicalPath(IFileSystem fileSystem)
-        {
-            return tempFile.Value.File.Path;
-        }
-
-        protected override SQLiteFormKeyAllocator CreateAllocator(IFileSystem fileSystem, IMod mod, string path)
-        {
-            if (fileSystem != IFileSystemExt.DefaultFilesystem)
-            {
-                throw new NotImplementedException();
-            }
-            return new(mod, path);
-        }
-
-        protected override SQLiteFormKeyAllocator CreateNamedAllocator(IFileSystem fileSystem, IMod mod, string path, string patcherName)
-        {
-            return new(mod, path, patcherName);
-        }
-
-        public void Dispose()
-        {
-            if (tempFile.IsValueCreated)
-                tempFile.Value.Dispose();
-            if (tempFolder.IsValueCreated)
-                tempFile.Value.Dispose();
-            GC.SuppressFinalize(this);
-        }
-    }
+    // public class SQLiteFormKeyAllocator_Tests : ISharedFormKeyAllocator_Tests<SQLiteFormKeyAllocator>, IDisposable
+    // {
+    //     protected override IFileSystem GetFileSystem() => IFileSystemExt.DefaultFilesystem;
+    //
+    //     protected Lazy<TempFolder> tempFolder;
+    //
+    //     protected Lazy<TempFile> tempFile;
+    //
+    //     private bool disposedValue;
+    //
+    //     public SQLiteFormKeyAllocator_Tests()
+    //     {
+    //         tempFolder = new(() => TempFolder.Factory());
+    //         tempFile = new(() => new TempFile(extraDirectoryPaths: TestPathing.TempFolderPath));
+    //     }
+    //     
+    //     protected override string ConstructTypicalPath(IFileSystem fileSystem)
+    //     {
+    //         return tempFile.Value.File.Path;
+    //     }
+    //
+    //     protected override SQLiteFormKeyAllocator CreateAllocator(IFileSystem fileSystem, IMod mod, string path)
+    //     {
+    //         if (fileSystem != IFileSystemExt.DefaultFilesystem)
+    //         {
+    //             throw new NotImplementedException();
+    //         }
+    //         return new(mod, path);
+    //     }
+    //
+    //     protected override SQLiteFormKeyAllocator CreateNamedAllocator(IFileSystem fileSystem, IMod mod, string path, string patcherName)
+    //     {
+    //         return new(mod, path, patcherName);
+    //     }
+    //
+    //     public void Dispose()
+    //     {
+    //         if (tempFile.IsValueCreated)
+    //             tempFile.Value.Dispose();
+    //         if (tempFolder.IsValueCreated)
+    //             tempFile.Value.Dispose();
+    //         GC.SuppressFinalize(this);
+    //     }
+    // }
 }
