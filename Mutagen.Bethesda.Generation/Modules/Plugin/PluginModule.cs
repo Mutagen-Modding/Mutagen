384cd266129eed52bd1856c7e3ca343f78f57780
Justin Swanson <justin.c.swanson@gmail.com>
2022-01-07 20:29:50 -0600

    Some cleaning

--- Mutagen.Bethesda.Generation/Modules/Plugin/PluginModule.cs
+++ Mutagen.Bethesda.Generation/Modules/Plugin/PluginModule.cs
@@ -9,97 +9,92 @@ using Mutagen.Bethesda.Generation.Modules.Plugin;
 using Mutagen.Bethesda.Generation.Modules.Binary;
 using Mutagen.Bethesda.Plugins.Records.Internals;
 
-namespace Mutagen.Bethesda.Generation.Modules
+namespace Mutagen.Bethesda.Generation.Modules;
+
+public class PluginModule : GenerationModule
 {
-    public class PluginModule : GenerationModule
+    public override string RegionString => "Mutagen";
+
+    public PluginModule()
     {
-        public override string RegionString => "Mutagen";
+        SubModules.Add(new TriggeringRecordModule());
+        SubModules.Add(new GenericsModule());
+        SubModules.Add(new VersioningModule());
+        SubModules.Add(new RecordTypeConverterModule());
+        SubModules.Add(new CorrectnessModule());
+        SubModules.Add(new ModModule());
+        SubModules.Add(new ColorTypeModule());
+        SubModules.Add(new LinkModule());
+        SubModules.Add(new MajorRecordModule());
+        SubModules.Add(new MajorRecordEnumerationModule());
+        SubModules.Add(new ContainerParentModule());
+        SubModules.Add(new MajorRecordFlagModule());
+        SubModules.Add(new DataTypeModule());
+        SubModules.Add(new LinkInterfaceModule());
+        SubModules.Add(new WarmupModule());
+        SubModules.Add(new MajorRecordRemovalModule());
+        SubModules.Add(new MajorRecordContextEnumerationModule());
+        SubModules.Add(new AspectInterfaceModule());
+        SubModules.Add(new TypeOptionSolidifier());
+        SubModules.Add(new LinkCacheExtensionsModule());
+        SubModules.Add(new DuplicateModule());
+        SubModules.Add(new GameCategoryExtensionsModule());
+        SubModules.Add(new InterfaceDocumentationModule());
+        SubModules.Add(new MapsToGetterModule());
+        SubModules.Add(new GameEnvironmentStateModule());
+        SubModules.Add(new MajorRecordLinkEqualityModule());
+        SubModules.Add(new ImplicitsModule());
+        SubModules.Add(new BreakMarkingModule());
+    }
 
-        public PluginModule()
+    public bool FieldFilter(TypeGeneration field)
+    {
+        var data = field.GetFieldData();
+        if (data.RecordType.HasValue
+            || field is NothingType or PrimitiveType or FormLinkType or ContainerType or DictType)
         {
-            this.SubModules.Add(new TriggeringRecordModule());
-            this.SubModules.Add(new GenericsModule());
-            this.SubModules.Add(new VersioningModule());
-            this.SubModules.Add(new RecordTypeConverterModule());
-            this.SubModules.Add(new CorrectnessModule());
-            this.SubModules.Add(new ModModule());
-            this.SubModules.Add(new ColorTypeModule());
-            this.SubModules.Add(new LinkModule());
-            this.SubModules.Add(new MajorRecordModule());
-            this.SubModules.Add(new MajorRecordEnumerationModule());
-            this.SubModules.Add(new ContainerParentModule());
-            this.SubModules.Add(new MajorRecordFlagModule());
-            this.SubModules.Add(new DataTypeModule());
-            this.SubModules.Add(new LinkInterfaceModule());
-            this.SubModules.Add(new WarmupModule());
-            this.SubModules.Add(new MajorRecordRemovalModule());
-            this.SubModules.Add(new MajorRecordContextEnumerationModule());
-            this.SubModules.Add(new AspectInterfaceModule());
-            this.SubModules.Add(new TypeOptionSolidifier());
-            this.SubModules.Add(new LinkCacheExtensionsModule());
-            this.SubModules.Add(new DuplicateModule());
-            this.SubModules.Add(new GameCategoryExtensionsModule());
-            this.SubModules.Add(new InterfaceDocumentationModule());
-            this.SubModules.Add(new MapsToGetterModule());
-            this.SubModules.Add(new GameEnvironmentStateModule());
-            this.SubModules.Add(new MajorRecordLinkEqualityModule());
-            this.SubModules.Add(new ImplicitsModule());
-            this.SubModules.Add(new BreakMarkingModule());
+            return true;
         }
 
-        public bool FieldFilter(TypeGeneration field)
+        if (field is GenderedType gender)
         {
-            var data = field.GetFieldData();
-            if (data.RecordType.HasValue
-                || field is NothingType
-                || field is PrimitiveType
-                || field is FormLinkType
-                || field is ContainerType
-                || field is DictType)
-            {
-                return true;
-            }
-
-            if (field is GenderedType gender)
-            {
-                return FieldFilter(gender.SubTypeGeneration);
-            }
-
-            return false;
+            return FieldFilter(gender.SubTypeGeneration);
         }
 
-        public override async Task PostFieldLoad(ObjectGeneration obj, TypeGeneration field, XElement node)
-        {
-            var data = field.CustomData.GetOrAdd(Constants.DataKey, () => new MutagenFieldData(field)) as MutagenFieldData;
-            data.Binary = node.GetAttribute<BinaryGenerationType>(Constants.Binary, BinaryGenerationType.Normal);
-            data.BinaryOverlay = node.GetAttribute<BinaryGenerationType?>(Constants.BinaryOverlay, default);
-            ModifyGRUPAttributes(field);
-            await base.PostFieldLoad(obj, field, node);
-            data.Length = node.GetAttribute<int?>(Constants.ByteLength, null);
-            if (data.Length.HasValue) return;
-            var index = obj.Fields.IndexOf(field);
-            if (field.IntegrateField 
-                && !FieldFilter(field)
-                && field.GetFieldData().Binary == BinaryGenerationType.Normal
-                && index != (obj.Fields.Count - 1))
-            {
-                throw new ArgumentException($"{obj.Name} {field.Name} have to define either length or record type.");
-            }
-        }
+        return false;
+    }
 
-        private void ModifyGRUPAttributes(TypeGeneration field)
+    public override async Task PostFieldLoad(ObjectGeneration obj, TypeGeneration field, XElement node)
+    {
+        var data = field.CustomData.GetOrAdd(Constants.DataKey, () => new MutagenFieldData(field)) as MutagenFieldData;
+        data.Binary = node.GetAttribute<BinaryGenerationType>(Constants.Binary, BinaryGenerationType.Normal);
+        data.BinaryOverlay = node.GetAttribute<BinaryGenerationType?>(Constants.BinaryOverlay, default);
+        ModifyGRUPAttributes(field);
+        await base.PostFieldLoad(obj, field, node);
+        data.Length = node.GetAttribute<int?>(Constants.ByteLength, null);
+        if (data.Length.HasValue) return;
+        var index = obj.Fields.IndexOf(field);
+        if (field.IntegrateField 
+            && !FieldFilter(field)
+            && field.GetFieldData().Binary == BinaryGenerationType.Normal
+            && index != (obj.Fields.Count - 1))
         {
-            if (!(field is LoquiType loqui)) return;
-            if (loqui.TargetObjectGeneration?.GetObjectType() != ObjectType.Group) return;
-            loqui.Singleton = true;
-            loqui.NullableProperty.OnNext((false, true));
-            loqui.NotifyingProperty.OnNext((NotifyingType.None, true));
+            throw new ArgumentException($"{obj.Name} {field.Name} have to define either length or record type.");
         }
+    }
 
-        public override async Task PostLoad(ObjectGeneration obj)
-        {
-            await base.PostLoad(obj);
-            obj.GetObjectData().CustomRecordFallback = obj.Node.GetAttribute(Constants.CustomFallback, false);
-        }
+    private void ModifyGRUPAttributes(TypeGeneration field)
+    {
+        if (field is not LoquiType loqui) return;
+        if (loqui.TargetObjectGeneration?.GetObjectType() != ObjectType.Group) return;
+        loqui.Singleton = true;
+        loqui.NullableProperty.OnNext((false, true));
+        loqui.NotifyingProperty.OnNext((NotifyingType.None, true));
+    }
+
+    public override async Task PostLoad(ObjectGeneration obj)
+    {
+        await base.PostLoad(obj);
+        obj.GetObjectData().CustomRecordFallback = obj.Node.GetAttribute(Constants.CustomFallback, false);
     }
 }
