c3d7b29105f3902cbf795924defe7dc2ce008d6e
Justin Swanson <justin.c.swanson@gmail.com>
2022-01-09 15:47:39 -0600

    Moved/cleaned some tests

---
+++ Mutagen.Bethesda.Core.UnitTests/Plugins/Binary/Streams/MutagenBinaryReadStreamTests.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+using FluentAssertions;
+using Mutagen.Bethesda.Core.UnitTests.AutoData;
+using Mutagen.Bethesda.Plugins;
+using Mutagen.Bethesda.Plugins.Binary.Streams;
+using Mutagen.Bethesda.Testing;
+using Xunit;
+using Noggog.Testing.IO;
+using Noggog.Utility;
+
+namespace Mutagen.Bethesda.UnitTests.Plugins.Binary.Streams;
+
+public class MutagenBinaryReadStreamTests
+{
+    [Theory, MutagenAutoData]
+    public void StreamModKeyCtorDoesNotDisposeStream()
+    {
+        var stream = new DisposeTesterWrapStream(File.OpenRead(TestDataPathing.SkyrimOverrideMod));
+        var mutaStream = new MutagenBinaryReadStream(
+            stream,
+            ModKey.FromNameAndExtension("Skyrim.esm"),
+            GameRelease.SkyrimSE);
+        stream.Disposed.Should().BeFalse();
+    }
+}
