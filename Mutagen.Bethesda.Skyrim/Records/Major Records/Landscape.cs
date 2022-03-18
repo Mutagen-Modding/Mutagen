ee60767abe4ceeb70af9b24e85a6d5a14569e139
Justin Swanson <justin.c.swanson@gmail.com>
2022-03-17 22:36:34 -0500

    Fix typo in Landscape.Flag.VertexNormalsHeightMap

--- Mutagen.Bethesda.Skyrim/Records/Major Records/Landscape.cs
+++ Mutagen.Bethesda.Skyrim/Records/Major Records/Landscape.cs
@@ -9,7 +9,7 @@ namespace Mutagen.Bethesda.Skyrim
         [Flags]
         public enum Flag
         {
-            VertexNormalsHightMap = 0x0001,
+            VertexNormalsHeightMap = 0x0001,
             VertexColors = 0x0002,
             Layers = 0x0004,
             MPCD = 0x0400,
