0ff28abb736fa6788c629f3375c649380eec5535
Justin Swanson <justin.c.swanson@gmail.com>
2022-03-20 14:14:19 -0500

    Xedit generation project

---
+++ Mutagen.Bethesda.XEdit.Generation/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Mutagen.Bethesda.XEdit.Generation;
+
+FunctionEnumGenerator.Convert(@"C:\Users\Levia\Downloads\new 10.txt", "output.txt");
