422cb0d00b6349e8bc83fa4eb77290f8f0fe5b6d
Justin Swanson <justin.c.swanson@gmail.com>
2022-05-02 17:20:49 -0500

    Warmup can only be run once

--- Mutagen.Bethesda.Core/Plugins/Warmup.cs
+++ Mutagen.Bethesda.Core/Plugins/Warmup.cs
@@ -10,27 +10,35 @@ namespace Mutagen.Bethesda.Plugins;
 /// </summary>
 public static class Warmup
 {
+    private static object _lock = new();
+    private static bool _warmedUp = false;
+
     /// <summary>
     /// Will initialize internal registrations.<br/>
     /// Not required to call, but can be used to warm up ahead of time.
     /// </summary>
     public static void Init()
     {
-        List<IProtocolRegistration> protocols = new()
+        lock (_lock)
         {
-            new ProtocolDefinition_Bethesda()
-        };
-        foreach (var category in EnumExt<GameCategory>.Values)
-        {
-            var obj = Activator.CreateInstance(
-                $"Mutagen.Bethesda.{category}",
-                $"Loqui.ProtocolDefinition_{category}");
-            var regis = obj?.Unwrap() as IProtocolRegistration;
-            if (regis == null) continue;
-            protocols.Add(regis);
+            if (_warmedUp) return;
+            List<IProtocolRegistration> protocols = new()
+            {
+                new ProtocolDefinition_Bethesda()
+            };
+            foreach (var category in EnumExt<GameCategory>.Values)
+            {
+                var obj = Activator.CreateInstance(
+                    $"Mutagen.Bethesda.{category}",
+                    $"Loqui.ProtocolDefinition_{category}");
+                var regis = obj?.Unwrap() as IProtocolRegistration;
+                if (regis == null) continue;
+                protocols.Add(regis);
+            }
+            Loqui.Initialization.SpinUp(protocols.ToArray());
+            MetaInterfaceMapping.Warmup();
+            OverrideMaskRegistrations.Warmup();
+            _warmedUp = true;
         }
-        Loqui.Initialization.SpinUp(protocols.ToArray());
-        MetaInterfaceMapping.Warmup();
-        OverrideMaskRegistrations.Warmup();
     }
 }
