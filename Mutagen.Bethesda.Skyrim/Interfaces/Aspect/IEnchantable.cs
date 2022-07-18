f3cfd81c66805423ee72f8858cd3e03636b10364
Justin Swanson <justin.c.swanson@gmail.com>
2022-07-18 17:36:45 -0500

    Skyrim IEnchantable

---
+++ Mutagen.Bethesda.Skyrim/Interfaces/Aspect/IEnchantable.cs
@@ -0,0 +1,15 @@
+﻿using Mutagen.Bethesda.Plugins;
+
+namespace Mutagen.Bethesda.Skyrim;
+
+public interface IEnchantable : ISkyrimMajorRecordInternal, IEnchantableGetter
+{
+    new IFormLinkNullable<IEffectRecordGetter> ObjectEffect { get; }
+    new ushort? EnchantmentAmount { get; set; }
+}
+
+public interface IEnchantableGetter : ISkyrimMajorRecordGetter
+{
+    IFormLinkNullableGetter<IEffectRecordGetter> ObjectEffect { get; }
+    ushort? EnchantmentAmount { get; }
+}
