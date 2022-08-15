551e52a925dc98e61a928ce9ebdc04786998768f
Justin Swanson <justin.c.swanson@gmail.com>
2022-08-15 18:07:49 -0500

    ILinkCache.CreateImmutableAssetLinkCache

---
+++ Mutagen.Bethesda.Core/Assets/AssetLinkCacheConstructionMixIn.cs
@@ -0,0 +1,17 @@
+﻿using Mutagen.Bethesda.Plugins.Cache;
+
+namespace Mutagen.Bethesda.Assets;
+
+public static class AssetLinkCacheConstructionMixIn
+{
+    /// <summary>
+    /// Creates an AssetLinkCache, which should only be used during sections of code where underlying records
+    /// do not change.
+    /// </summary>
+    /// <param name="linkCache">LinkCache to resolve against for Resolved assets</param>
+    /// <returns>An AssetLinkCache relative to the given ILinkCache</returns>
+    public static IAssetLinkCache CreateImmutableAssetLinkCache(this ILinkCache linkCache)
+    {
+        return new AssetLinkCache(linkCache);
+    }
+}
