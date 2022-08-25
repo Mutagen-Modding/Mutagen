ab9ef16532ffb56477cfdda1f23eab7be2104e4e
nickp <nickposer2102@gmail.com>
2022-08-25 11:30:36 +0200

    Fixed crash when Prep tries to run twice

--- Mutagen.Bethesda.Skyrim/Records/Assets/VoiceType/VoiceTypeAssetLookup.cs
+++ Mutagen.Bethesda.Skyrim/Records/Assets/VoiceType/VoiceTypeAssetLookup.cs
@@ -28,6 +28,8 @@ public class VoiceTypeAssetLookup : IAssetCacheComponent
 
     public void Prep(IAssetLinkCache linkCache)
     {
+        if (linkCache.FormLinkCache == _formLinkCache) return;
+
         _formLinkCache = linkCache.FormLinkCache;
 
         var childRaces = new HashSet<FormKey>();
