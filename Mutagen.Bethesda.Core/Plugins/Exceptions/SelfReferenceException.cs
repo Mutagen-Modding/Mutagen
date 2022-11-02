efbb1f8d7be5d334847199eb6e26c61d272cda48
Justin Swanson <justin.c.swanson@gmail.com>
2022-11-01 19:42:41 -0500

    TooManyMastersException SelfReferenceException

---
+++ Mutagen.Bethesda.Core/Plugins/Exceptions/SelfReferenceException.cs
@@ -0,0 +1,12 @@
+﻿namespace Mutagen.Bethesda.Plugins.Exceptions;
+
+public class SelfReferenceException : Exception
+{
+    public ModKey ModKey { get; }
+
+    public SelfReferenceException(ModKey modKey)
+        : base($"Cannot add mod to itself as a master: {modKey}")
+    {
+        ModKey = modKey;
+    }
+}
---
+++ Mutagen.Bethesda.Core/Plugins/Exceptions/TooManyMastersException.cs
@@ -0,0 +1,21 @@
+﻿using Mutagen.Bethesda.Plugins.Internals;
+
+namespace Mutagen.Bethesda.Plugins.Exceptions;
+
+public class TooManyMastersException : Exception
+{
+    public ModKey CurrentMod { get; }
+    
+    private readonly ModKey[] _masters;
+
+    public IReadOnlyList<ModKey> Masters => _masters;
+
+    public TooManyMastersException(
+        ModKey currentMod,
+        ModKey[] masters)
+        : base($"{currentMod} has too many masters on masters list. {masters.Length} >= {Constants.PluginMasterLimit}.")
+    {
+        CurrentMod = currentMod;
+        _masters = masters;
+    }
+}
