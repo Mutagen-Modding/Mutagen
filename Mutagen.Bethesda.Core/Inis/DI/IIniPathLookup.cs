46de0ce5c1242b4c6b31cecf5492323de40506e8
Justin Swanson <justin.c.swanson@gmail.com>
2022-11-19 22:07:49 -0600

    Some INI DI objects

---
+++ Mutagen.Bethesda.Core/Inis/DI/IIniPathLookup.cs
@@ -0,0 +1,46 @@
+﻿using Noggog;
+
+namespace Mutagen.Bethesda.Inis.DI;
+
+public interface IIniPathLookup
+{
+    FilePath Get(GameRelease release);
+}
+
+public class IniPathLookup : IIniPathLookup
+{
+    public FilePath Get(GameRelease release)
+    {
+        return Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "My Games", ToMyDocumentsString(release), $"{ToIniName(release)}.ini");
+    }
+
+    public static string ToMyDocumentsString(GameRelease release)
+    {
+        return release switch
+        {
+            GameRelease.Oblivion => "Oblivion",
+            GameRelease.SkyrimLE => "Skyrim",
+            GameRelease.EnderalLE => "Enderal",
+            GameRelease.SkyrimSE => "Skyrim Special Edition",
+            GameRelease.EnderalSE => "Enderal Special Edition",
+            GameRelease.SkyrimVR => "Skyrim VR",
+            GameRelease.Fallout4 => "Fallout4",
+            _ => throw new NotImplementedException(),
+        };
+    }
+
+    public static string ToIniName(GameRelease release)
+    {
+        return release switch
+        {
+            GameRelease.Oblivion => "Oblivion",
+            GameRelease.SkyrimLE => "Skyrim",
+            GameRelease.SkyrimSE => "Skyrim",
+            GameRelease.EnderalLE => "Enderal",
+            GameRelease.EnderalSE => "Enderal",
+            GameRelease.SkyrimVR => "SkyrimVR",
+            GameRelease.Fallout4 => "Fallout4",
+            _ => throw new NotImplementedException(),
+        };
+    }
+}
---
+++ Mutagen.Bethesda.Core/Inis/DI/IIniPathProvider.cs
@@ -0,0 +1,27 @@
+﻿using Mutagen.Bethesda.Environments.DI;
+using Noggog;
+
+namespace Mutagen.Bethesda.Inis.DI;
+
+public interface IIniPathProvider
+{
+    FilePath Path { get; }
+}
+
+public sealed class IniPathProvider : IIniPathProvider
+{
+    private readonly IGameReleaseContext _releaseContext;
+    private readonly IIniPathLookup _lookup;
+    
+    public FilePath Path => _lookup.Get(_releaseContext.Release);
+
+    public IniPathProvider(
+        IGameReleaseContext releaseContext,
+        IIniPathLookup lookup)
+    {
+        _releaseContext = releaseContext;
+        _lookup = lookup;
+    }
+}
+
+public record IniPathInjection(FilePath Path) : IIniPathProvider;
