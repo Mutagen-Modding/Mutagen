955881371da8329d269b6d9b48ef59e06e510b5c
Justin Swanson <justin.c.swanson@gmail.com>
2023-06-23 11:36:20 -0500

    Some groundwork for ActorValueMapper

---
+++ Mutagen.Bethesda.Skyrim/Enums/ActorValueMapper.cs
@@ -0,0 +1,22 @@
+﻿using Mutagen.Bethesda.Plugins;
+
+namespace Mutagen.Bethesda.Skyrim;
+
+public static class ActorValueMapper
+{
+    private static readonly IReadOnlyDictionary<FormKey, ActorValue> _mapping;
+
+    static ActorValueMapper()
+    {
+        _mapping = new Dictionary<FormKey, ActorValue>()
+        {
+            { FormKey.Factory("0123456:Skyrim.esm"), ActorValue.Aggression },
+            // ...
+        };
+    }
+
+    public static bool TryGetActorValue(IActorValueInformationGetter actorValueInformation, out ActorValue actorValue)
+    {
+        return _mapping.TryGetValue(actorValueInformation.FormKey, out actorValue);
+    }
+}
