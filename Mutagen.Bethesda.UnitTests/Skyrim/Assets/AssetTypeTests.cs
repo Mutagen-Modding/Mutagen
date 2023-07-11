5ca34dbfaf618f722a06ec7096cb34be51223c4f
nickp <nickposer2102@gmail.com>
2023-07-11 22:19:44 +0200

    Add asset type tests

---
+++ Mutagen.Bethesda.UnitTests/Skyrim/Assets/AssetTypeTests.cs
@@ -0,0 +1,44 @@
+﻿using FluentAssertions;
+using Mutagen.Bethesda.Assets;
+using Noggog;
+using Xunit;
+
+namespace Mutagen.Bethesda.UnitTests.Skyrim.Assets;
+
+public class AssetTypeTests
+{
+#if NET7_0_OR_GREATER
+    public IAssetType GetInstance<TAssetType>()
+        where TAssetType : class, IAssetType
+    {
+        return TAssetType.Instance;
+    }
+
+    [Fact]
+    public void TestAllImplementationsAreNotNull()
+    {
+        var methodInfo = typeof(AssetTypeTests).GetMethod("GetInstance");
+
+        foreach (var implementation in typeof(IAssetType).GetInheritingFromInterface())
+        {
+            var method = methodInfo?.MakeGenericMethod(implementation);
+
+            var instance = method?.Invoke(this, null);
+
+            instance.Should().NotBeNull();
+        }
+
+    }
+
+    [Fact]
+    public void TestAllImplementationsHaveNoBaseClass()
+    {
+        var objectType = typeof(object);
+        
+        foreach (var implementation in typeof(IAssetType).GetInheritingFromInterface())
+        {
+            implementation.BaseType.Should().Be(objectType);
+        }
+    }
+#endif
+}
