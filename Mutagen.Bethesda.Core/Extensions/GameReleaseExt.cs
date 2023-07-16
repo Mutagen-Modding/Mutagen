1f320c21f800ef06a7980939facf7aaffb8b1aa5
Justin Swanson <justin.c.swanson@gmail.com>
2023-07-16 00:17:40 -0500

    ToCategory ext function moved to kernel

--- Mutagen.Bethesda.Core/Extensions/GameReleaseExt.cs
+++ Mutagen.Bethesda.Core/Extensions/GameReleaseExt.cs
@@ -4,38 +4,6 @@ namespace Mutagen.Bethesda;
 
 public static class GameReleaseExt
 {
-    public static GameCategory ToCategory(this GameRelease release)
-    {
-        return release switch
-        {
-            GameRelease.Oblivion => GameCategory.Oblivion,
-            GameRelease.SkyrimLE => GameCategory.Skyrim,
-            GameRelease.SkyrimSE => GameCategory.Skyrim,
-            GameRelease.SkyrimSEGog => GameCategory.Skyrim,
-            GameRelease.SkyrimVR => GameCategory.Skyrim,
-            GameRelease.EnderalLE => GameCategory.Skyrim,
-            GameRelease.EnderalSE => GameCategory.Skyrim,
-            GameRelease.Fallout4 => GameCategory.Fallout4,
-            _ => throw new NotImplementedException(),
-        };
-    }
-
-    public static ushort? GetDefaultFormVersion(this GameRelease release)
-    {
-        return release switch
-        {
-            GameRelease.Oblivion => default,
-            GameRelease.SkyrimLE => 43,
-            GameRelease.EnderalLE => 43,
-            GameRelease.SkyrimSE => 44,
-            GameRelease.SkyrimSEGog => 44,
-            GameRelease.EnderalSE => 44,
-            GameRelease.SkyrimVR => 44,
-            GameRelease.Fallout4 => 131,
-            _ => throw new NotImplementedException(),
-        };
-    }
-
     public static StringsLanguageFormat GetLanguageFormat(this GameRelease release)
     {
         switch (release)
