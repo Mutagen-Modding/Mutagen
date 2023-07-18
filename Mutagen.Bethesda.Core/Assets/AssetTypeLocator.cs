27e6f102d0554c38a0cdfee27b2d06b7b49bcde5
nickp <nickposer2102@gmail.com>
2023-07-18 20:01:09 +0200

    Add GetAssetType that throws on null

--- Mutagen.Bethesda.Core/Assets/AssetTypeLocator.cs
+++ Mutagen.Bethesda.Core/Assets/AssetTypeLocator.cs
@@ -48,6 +48,23 @@ public class AssetTypeLocator
 	/// </summary>
 	/// <param name="gameCategory">Release of the game this asset comes from</param>
 	/// <param name="path">Path of the asset</param>
+	/// <returns>Instance of the parsed asset type</returns>
+	/// <exception cref="ArgumentException">When the asset type couldn't be determined</exception>
+	public static IAssetType GetAssetType(GameCategory gameCategory, string path) {
+		var assetType = TryGetGetAssetType(gameCategory, path);
+
+		if (assetType is null) {
+			throw new ArgumentException($"Could not determine asset type for {path} in {gameCategory}");
+		}
+
+		return assetType;
+	}
+
+	/// <summary>
+	/// Parse asset type by game release and path
+	/// </summary>
+	/// <param name="gameCategory">Release of the game this asset comes from</param>
+	/// <param name="path">Path of the asset</param>
 	/// <returns>Instance of the parsed asset type or null if no asset type could be determined</returns>
 	public static IAssetType? TryGetGetAssetType(GameCategory gameCategory, string path)
 	{
