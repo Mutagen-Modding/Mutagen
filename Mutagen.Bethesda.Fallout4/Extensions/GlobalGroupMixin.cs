3897f979628d4d3246f8efadbc87c02e0a2df99e
Mark <1515135+MarkKoz@users.noreply.github.com>
2023-08-21 01:13:49 -0700

    Add GlobalGroupMixin for Fallout 4

---
+++ Mutagen.Bethesda.Fallout4/Extensions/GlobalGroupMixin.cs
@@ -0,0 +1,70 @@
+﻿using Mutagen.Bethesda.Plugins.Records;
+
+namespace Mutagen.Bethesda.Fallout4;
+
+public static class GlobalGroupMixIn
+{
+    public static GlobalBool AddNewBool(this IGroup<Global> globals)
+    {
+        var ret = new GlobalBool(
+            globals.SourceMod.GetNextFormKey());
+        globals.Add(ret);
+        return ret;
+    }
+
+    public static GlobalBool AddNewBool(this IGroup<Global> globals, string editorId)
+    {
+        var ret = new GlobalBool(
+            globals.SourceMod.GetNextFormKey(editorId));
+        globals.Add(ret);
+        return ret;
+    }
+    
+    public static GlobalFloat AddNewFloat(this IGroup<Global> globals)
+    {
+        var ret = new GlobalFloat(
+            globals.SourceMod.GetNextFormKey());
+        globals.Add(ret);
+        return ret;
+    }
+
+    public static GlobalFloat AddNewFloat(this IGroup<Global> globals, string editorId)
+    {
+        var ret = new GlobalFloat(
+            globals.SourceMod.GetNextFormKey(editorId));
+        globals.Add(ret);
+        return ret;
+    }
+
+    public static GlobalInt AddNewInt(this IGroup<Global> globals)
+    {
+        var ret = new GlobalInt(
+            globals.SourceMod.GetNextFormKey());
+        globals.Add(ret);
+        return ret;
+    }
+
+    public static GlobalInt AddNewInt(this IGroup<Global> globals, string editorId)
+    {
+        var ret = new GlobalInt(
+            globals.SourceMod.GetNextFormKey(editorId));
+        globals.Add(ret);
+        return ret;
+    }
+
+    public static GlobalShort AddNewShort(this IGroup<Global> globals)
+    {
+        var ret = new GlobalShort(
+            globals.SourceMod.GetNextFormKey());
+        globals.Add(ret);
+        return ret;
+    }
+
+    public static GlobalShort AddNewShort(this IGroup<Global> globals, string editorId)
+    {
+        var ret = new GlobalShort(
+            globals.SourceMod.GetNextFormKey(editorId));
+        globals.Add(ret);
+        return ret;
+    }
+}
