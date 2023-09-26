720586ee8fea181cb71b836b4a3ef059d5a78ed5
Justin Swanson <justin.c.swanson@gmail.com>
2023-09-26 18:11:12 -0500

    TimeOfDay enum

--- Mutagen.Bethesda.Starfield/Records/Major Records/WeatherAmbientColorSet.cs
+++ Mutagen.Bethesda.Starfield/Records/Major Records/WeatherAmbientColorSet.cs
@@ -2,77 +2,77 @@ namespace Mutagen.Bethesda.Starfield;
 
 partial class WeatherAmbientColorSet
 {
-    // IAmbientColorsGetter IWeatherAmbientColorSetGetter.this[TimeOfDay time] => time switch
-    // {
-    //     TimeOfDay.Sunrise => this.Sunrise,
-    //     TimeOfDay.Day => this.Day,
-    //     TimeOfDay.Night => this.Night,
-    //     TimeOfDay.Sunset => this.Sunset,
-    //     TimeOfDay.EarlySunrise => this.EarlySunrise,
-    //     TimeOfDay.LateSunrise => this.LateSunrise,
-    //     TimeOfDay.EarlySunset => this.EarlySunset,
-    //     TimeOfDay.LateSunset => this.LateSunset,
-    //     _ => throw new NotImplementedException(),
-    // };
+    IAmbientColorsGetter IWeatherAmbientColorSetGetter.this[TimeOfDay time] => time switch
+    {
+        TimeOfDay.Sunrise => this.Sunrise,
+        TimeOfDay.Day => this.Day,
+        TimeOfDay.Night => this.Night,
+        TimeOfDay.Sunset => this.Sunset,
+        TimeOfDay.EarlySunrise => this.EarlySunrise,
+        TimeOfDay.LateSunrise => this.LateSunrise,
+        TimeOfDay.EarlySunset => this.EarlySunset,
+        TimeOfDay.LateSunset => this.LateSunset,
+        _ => throw new NotImplementedException(),
+    };
 
-    // public AmbientColors this[TimeOfDay time]
-    // {
-    //     get => time switch
-    //     {
-    //         TimeOfDay.Sunrise => this.Sunrise,
-    //         TimeOfDay.Day => this.Day,
-    //         TimeOfDay.Night => this.Night,
-    //         TimeOfDay.Sunset => this.Sunset,
-    //         TimeOfDay.EarlySunrise => this.EarlySunrise,
-    //         TimeOfDay.LateSunrise => this.LateSunrise,
-    //         TimeOfDay.EarlySunset => this.EarlySunset,
-    //         TimeOfDay.LateSunset => this.LateSunset,
-    //         _ => throw new NotImplementedException(),
-    //     };
-    //
-    //     set
-    //     {
-    //         switch (time)
-    //         {
-    //             case TimeOfDay.Sunrise:
-    //                 this.Sunrise = value;
-    //                 break;
-    //             case TimeOfDay.Day:
-    //                 this.Day = value;
-    //                 break;
-    //             case TimeOfDay.Sunset:
-    //                 this.Sunset = value;
-    //                 break;
-    //             case TimeOfDay.Night:
-    //                 this.Night = value;
-    //                 break;
-    //             case TimeOfDay.EarlySunrise:
-    //                 this.EarlySunrise = value;
-    //                 break;
-    //             case TimeOfDay.LateSunrise:
-    //                 this.LateSunrise = value;
-    //                 break;
-    //             case TimeOfDay.EarlySunset:
-    //                 this.EarlySunset = value;
-    //                 break;
-    //             case TimeOfDay.LateSunset:
-    //                 this.LateSunset = value;
-    //                 break;
-    //             default:
-    //                 throw new NotImplementedException();
-    //         }
-    //     }
-    // }
+    public AmbientColors this[TimeOfDay time]
+    {
+        get => time switch
+        {
+            TimeOfDay.Sunrise => this.Sunrise,
+            TimeOfDay.Day => this.Day,
+            TimeOfDay.Night => this.Night,
+            TimeOfDay.Sunset => this.Sunset,
+            TimeOfDay.EarlySunrise => this.EarlySunrise,
+            TimeOfDay.LateSunrise => this.LateSunrise,
+            TimeOfDay.EarlySunset => this.EarlySunset,
+            TimeOfDay.LateSunset => this.LateSunset,
+            _ => throw new NotImplementedException(),
+        };
+    
+        set
+        {
+            switch (time)
+            {
+                case TimeOfDay.Sunrise:
+                    this.Sunrise = value;
+                    break;
+                case TimeOfDay.Day:
+                    this.Day = value;
+                    break;
+                case TimeOfDay.Sunset:
+                    this.Sunset = value;
+                    break;
+                case TimeOfDay.Night:
+                    this.Night = value;
+                    break;
+                case TimeOfDay.EarlySunrise:
+                    this.EarlySunrise = value;
+                    break;
+                case TimeOfDay.LateSunrise:
+                    this.LateSunrise = value;
+                    break;
+                case TimeOfDay.EarlySunset:
+                    this.EarlySunset = value;
+                    break;
+                case TimeOfDay.LateSunset:
+                    this.LateSunset = value;
+                    break;
+                default:
+                    throw new NotImplementedException();
+            }
+        }
+    }
 }
 
 public partial interface IWeatherAmbientColorSet
 {
-    // new AmbientColors this[TimeOfDay time] { get; set; }
+    new AmbientColors this[TimeOfDay time] { get; set; }
 }
 
 public partial interface IWeatherAmbientColorSetGetter
 {
-    // IAmbientColorsGetter this[TimeOfDay time] { get; }
+    IAmbientColorsGetter this[TimeOfDay time] { get; }
 }
 
 partial class WeatherAmbientColorSetBinaryOverlay
@@ -92,16 +92,16 @@ partial class WeatherAmbientColorSetBinaryOverlay
 
     public IAmbientColorsGetter LateSunset => throw new NotImplementedException();
 
-    // public IAmbientColorsGetter this[TimeOfDay time] => time switch
-    // {
-    //     TimeOfDay.Sunrise => this.Sunrise,
-    //     TimeOfDay.Day => this.Day,
-    //     TimeOfDay.Night => this.Night,
-    //     TimeOfDay.Sunset => this.Sunset,
-    //     TimeOfDay.EarlySunrise => this.EarlySunrise,
-    //     TimeOfDay.LateSunrise => this.LateSunrise,
-    //     TimeOfDay.EarlySunset => this.EarlySunset,
-    //     TimeOfDay.LateSunset => this.LateSunset,
-    //     _ => throw new NotImplementedException(),
-    // };
+    public IAmbientColorsGetter this[TimeOfDay time] => time switch
+    {
+        TimeOfDay.Sunrise => this.Sunrise,
+        TimeOfDay.Day => this.Day,
+        TimeOfDay.Night => this.Night,
+        TimeOfDay.Sunset => this.Sunset,
+        TimeOfDay.EarlySunrise => this.EarlySunrise,
+        TimeOfDay.LateSunrise => this.LateSunrise,
+        TimeOfDay.EarlySunset => this.EarlySunset,
+        TimeOfDay.LateSunset => this.LateSunset,
+        _ => throw new NotImplementedException(),
+    };
 }
