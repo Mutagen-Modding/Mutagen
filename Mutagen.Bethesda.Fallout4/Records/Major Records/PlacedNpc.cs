5553c4c4f2b6c5ade46da586bfbe4114bc352463
Justin Swanson <justin.c.swanson@gmail.com>
2023-12-01 23:56:50 -0600

    Fo4 PlacedNpc.StartsUnconscious int value fix

--- Mutagen.Bethesda.Fallout4/Records/Major Records/PlacedNpc.cs
+++ Mutagen.Bethesda.Fallout4/Records/Major Records/PlacedNpc.cs
@@ -8,7 +8,7 @@ partial class PlacedNpc
         StartsDead = 0x0000_0200,
         Persistent = 0x0000_0400,
         InitiallyDisabled = 0x0000_0800,
-        StartsUnconscious = 0x0000_2800,
+        StartsUnconscious = 0x0000_2000,
         NoAiAcquire = 0x0200_0000,
         DoNotHavokSettle = 0x2000_0000,
     }
