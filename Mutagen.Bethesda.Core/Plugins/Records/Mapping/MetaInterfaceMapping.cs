dcaa57bd1fc9905a6de83c12418cbff5ec3a0a88
Justin Swanson <justin.c.swanson@gmail.com>
2024-01-23 03:33:10 -0600

    MetaInterfaceMapper call w/o category.  Tests

--- Mutagen.Bethesda.Core/Plugins/Records/Mapping/MetaInterfaceMapping.cs
+++ Mutagen.Bethesda.Core/Plugins/Records/Mapping/MetaInterfaceMapping.cs
@@ -1,10 +1,13 @@
 ﻿using System.Diagnostics.CodeAnalysis;
+using Noggog;
 
 namespace Mutagen.Bethesda.Plugins.Records.Mapping;
 
 public interface IMetaInterfaceMapGetter
 {
     bool TryGetRegistrationsForInterface(GameCategory mode, Type type,
+        [MaybeNullWhen(false)] out InterfaceMappingResult registrations);
+    bool TryGetRegistrationsForInterface(Type type,
         [MaybeNullWhen(false)] out InterfaceMappingResult registrations);
 }
 
@@ -14,7 +17,9 @@ internal sealed class MetaInterfaceMapper : IMetaInterfaceMapGetter
     private readonly ILinkInterfaceMapGetter _linkInterfaceMapGetter;
     private readonly IInheritingInterfaceMapGetter _inheritingInterfaceMapGetter;
     private readonly IIsolatedAbstractInterfaceMapGetter _abstractInterfaceMapGetter;
-
+    private readonly Lazy<Dictionary<GameCategory, Dictionary<Type, InterfaceMappingResult>>> _categoryTypeDict;
+    private readonly Lazy<Dictionary<Type, InterfaceMappingResult>> _typeDict;
+    
     public MetaInterfaceMapper(
         IAspectInterfaceMapGetter aspectInterfaceMapGetter,
         ILinkInterfaceMapGetter linkInterfaceMapGetter,
@@ -25,15 +30,51 @@ internal sealed class MetaInterfaceMapper : IMetaInterfaceMapGetter
         _linkInterfaceMapGetter = linkInterfaceMapGetter;
         _inheritingInterfaceMapGetter = inheritingInterfaceMapGetter;
         _abstractInterfaceMapGetter = abstractInterfaceMapGetter;
+        _categoryTypeDict = new Lazy<Dictionary<GameCategory, Dictionary<Type, InterfaceMappingResult>>>(() =>
+        {
+            var ret = new Dictionary<GameCategory, Dictionary<Type, InterfaceMappingResult>>();
+            foreach (var cat in Enums<GameCategory>.Values)
+            {
+                ret[cat] = _aspectInterfaceMapGetter.InterfaceToObjectTypes(cat)
+                    .Concat(_linkInterfaceMapGetter.InterfaceToObjectTypes(cat))
+                    .Concat(_inheritingInterfaceMapGetter.InterfaceToObjectTypes(cat))
+                    .Concat(_abstractInterfaceMapGetter.InterfaceToObjectTypes(cat))
+                    .ToDictionary(x => x.Key, x => x.Value);
+            }
+            return ret;
+        });
+        _typeDict = new Lazy<Dictionary<Type, InterfaceMappingResult>>(() =>
+        {
+            var ret = new Dictionary<Type, InterfaceMappingResult>();
+            foreach (var kv in _categoryTypeDict.Value
+                         .SelectMany(x => x.Value))
+            {
+                if (!ret.TryGetValue(kv.Key, out var items))
+                {
+                    ret[kv.Key] = kv.Value with { Registrations = kv.Value.Registrations.ToList() };
+                }
+                else
+                {
+                    ret[kv.Key] = items with { Registrations = items.Registrations.Concat(kv.Value.Registrations).ToList() };
+                }
+            }
+            return ret;
+        });
     }
 
     public bool TryGetRegistrationsForInterface(GameCategory mode, Type type, [MaybeNullWhen(false)] out InterfaceMappingResult registrations)
     {
-        if (_aspectInterfaceMapGetter.InterfaceToObjectTypes(mode).TryGetValue(type, out registrations)) return true;
-        if (_linkInterfaceMapGetter.InterfaceToObjectTypes(mode).TryGetValue(type, out registrations)) return true;
-        if (_inheritingInterfaceMapGetter.InterfaceToObjectTypes(mode).TryGetValue(type, out registrations)) return true;
-        if (_abstractInterfaceMapGetter.InterfaceToObjectTypes(mode).TryGetValue(type, out registrations)) return true;
-        return false;
+        if (!_categoryTypeDict.Value.TryGetValue(mode, out var subDict))
+        {
+            registrations = default;
+            return false;
+        }
+        return subDict.TryGetValue(type, out registrations);
+    }
+
+    public bool TryGetRegistrationsForInterface(Type type, [MaybeNullWhen(false)] out InterfaceMappingResult registrations)
+    {
+        return _typeDict.Value.TryGetValue(type, out registrations);
     }
 }
 
---
+++ Mutagen.Bethesda.UnitTests/Plugins/Records/MetaInterfaceMapperTests.cs
@@ -0,0 +1,34 @@
+﻿using FluentAssertions;
+using Mutagen.Bethesda.Plugins.Records.Mapping;
+using Mutagen.Bethesda.Skyrim;
+using Xunit;
+
+namespace Mutagen.Bethesda.UnitTests.Plugins.Records;
+
+public class MetaInterfaceMapperTests
+{
+    [Fact]
+    internal void Typical()
+    {
+        MetaInterfaceMapping.Instance.TryGetRegistrationsForInterface(typeof(IWorldspaceOrListGetter), out var regis)
+            .Should().BeTrue();
+        regis.Setter.Should().BeFalse();
+        regis.Registrations.Select(x => x.GetterType).Should().Contain(typeof(IWorldspaceGetter));
+    }
+    
+    [Fact]
+    internal void ByCategory()
+    {
+        MetaInterfaceMapping.Instance.TryGetRegistrationsForInterface(GameCategory.Skyrim, typeof(IWorldspaceOrListGetter), out var regis)
+            .Should().BeTrue();
+        regis.Setter.Should().BeFalse();
+        regis.Registrations.Select(x => x.GetterType).Should().Contain(typeof(IWorldspaceGetter));
+    }
+    
+    [Fact]
+    internal void ByCategoryMiss()
+    {
+        MetaInterfaceMapping.Instance.TryGetRegistrationsForInterface(GameCategory.Oblivion, typeof(IWorldspaceOrListGetter), out var regis)
+            .Should().BeFalse();
+    }
+}
