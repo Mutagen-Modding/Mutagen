240337fe8de3f96a37d8a16ac7357d88a113c55d
Elscrux <nickposer2102@gmail.com>
2024-04-02 17:25:28 +0200

    Add MajorRecordTypeEnumerator

---
+++ Mutagen.Bethesda.Core/Plugins/Records/Mapping/MajorRecordTypeEnumerator.cs
@@ -0,0 +1,32 @@
+﻿using Loqui;
+using Noggog;
+namespace Mutagen.Bethesda.Plugins.Records.Mapping;
+
+public static class MajorRecordTypeEnumerator {
+	private static readonly Dictionary<GameCategory, List<ILoquiRegistration>> Registrations = new();
+
+	public static List<ILoquiRegistration> GetRegistrations(GameCategory cat) {
+		var gameNamespace = "Mutagen.Bethesda." + cat;
+
+		return LoquiRegistration.StaticRegister.Registrations
+			.Where(x =>
+				string.Equals(x.ClassType.Namespace, gameNamespace, StringComparison.Ordinal)
+			 && x.GetterType.IsAssignableTo(typeof(IMajorRecordGetter)))
+			.ToList();
+	}
+
+	IEnumerable<Type> GetMajorRecordClassTypesFor(GameCategory cat) {
+		return Registrations.GetOrAdd(cat, () => GetRegistrations(cat))
+			.Select(x => x.ClassType);
+	}
+
+	IEnumerable<Type> GetMajorRecordGetterTypesFor(GameCategory cat) {
+		return Registrations.GetOrAdd(cat, () => GetRegistrations(cat))
+			.Select(x => x.GetterType);
+	}
+
+	IEnumerable<Type> GetMajorRecordSetterTypesFor(GameCategory cat) {
+		return Registrations.GetOrAdd(cat, () => GetRegistrations(cat))
+			.Select(x => x.SetterType);
+	}
+}
