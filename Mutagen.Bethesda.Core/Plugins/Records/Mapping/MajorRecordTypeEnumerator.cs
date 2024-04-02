f039ee01c7c234eed11eff14ebdad083429dca70
Elscrux <nickposer2102@gmail.com>
2024-04-02 17:27:48 +0200

    Code style

--- Mutagen.Bethesda.Core/Plugins/Records/Mapping/MajorRecordTypeEnumerator.cs
+++ Mutagen.Bethesda.Core/Plugins/Records/Mapping/MajorRecordTypeEnumerator.cs
@@ -2,10 +2,12 @@
 using Noggog;
 namespace Mutagen.Bethesda.Plugins.Records.Mapping;
 
-public static class MajorRecordTypeEnumerator {
+public static class MajorRecordTypeEnumerator
+{
 	private static readonly Dictionary<GameCategory, List<ILoquiRegistration>> Registrations = new();
 
-	public static List<ILoquiRegistration> GetRegistrations(GameCategory cat) {
+	public static List<ILoquiRegistration> GetRegistrations(GameCategory cat)
+	{
 		var gameNamespace = "Mutagen.Bethesda." + cat;
 
 		return LoquiRegistration.StaticRegister.Registrations
@@ -15,21 +17,25 @@ public static class MajorRecordTypeEnumerator {
 			.ToList();
 	}
 
-	IEnumerable<ILoquiRegistration> GetMajorRecordRegistrationsFor(GameCategory cat) {
+	IEnumerable<ILoquiRegistration> GetMajorRecordRegistrationsFor(GameCategory cat)
+	{
 		return Registrations.GetOrAdd(cat, () => GetRegistrations(cat));
 	}
 
-	IEnumerable<Type> GetMajorRecordClassTypesFor(GameCategory cat) {
+	IEnumerable<Type> GetMajorRecordClassTypesFor(GameCategory cat)
+	{
 		return Registrations.GetOrAdd(cat, () => GetRegistrations(cat))
 			.Select(x => x.ClassType);
 	}
 
-	IEnumerable<Type> GetMajorRecordGetterTypesFor(GameCategory cat) {
+	IEnumerable<Type> GetMajorRecordGetterTypesFor(GameCategory cat)
+	{
 		return Registrations.GetOrAdd(cat, () => GetRegistrations(cat))
 			.Select(x => x.GetterType);
 	}
 
-	IEnumerable<Type> GetMajorRecordSetterTypesFor(GameCategory cat) {
+	IEnumerable<Type> GetMajorRecordSetterTypesFor(GameCategory cat)
+	{
 		return Registrations.GetOrAdd(cat, () => GetRegistrations(cat))
 			.Select(x => x.SetterType);
 	}
