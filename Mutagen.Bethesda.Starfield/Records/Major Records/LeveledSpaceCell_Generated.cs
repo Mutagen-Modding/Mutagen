1acf7754f4b48f17a30a06613cc98ff8545b8343
Justin Swanson <justin.c.swanson@gmail.com>
2024-05-17 16:39:53 -0500

    Starfield LeveledSpaceCell ChanceNone upgrade

--- Mutagen.Bethesda.Starfield/Records/Major Records/LeveledSpaceCell_Generated.cs
+++ Mutagen.Bethesda.Starfield/Records/Major Records/LeveledSpaceCell_Generated.cs
@@ -1605,10 +1605,9 @@ namespace Mutagen.Bethesda.Starfield
                 item: item,
                 writer: writer,
                 translationParams: translationParams);
-            FloatBinaryTranslation<MutagenFrame, MutagenWriter>.Instance.Write(
+            LeveledSpaceCellBinaryWriteTranslation.WriteBinaryChanceNone(
                 writer: writer,
-                item: item.ChanceNone,
-                header: translationParams.ConvertToCustom(RecordTypes.LVLD));
+                item: item);
             ByteBinaryTranslation<MutagenFrame, MutagenWriter>.Instance.Write(
                 writer: writer,
                 item: item.MaxCount,
@@ -1635,6 +1634,19 @@ namespace Mutagen.Bethesda.Starfield
                         writer: subWriter,
                         translationParams: conv);
                 });
+        }
+
+        public static partial void WriteBinaryChanceNoneCustom(
+            MutagenWriter writer,
+            ILeveledSpaceCellGetter item);
+
+        public static void WriteBinaryChanceNone(
+            MutagenWriter writer,
+            ILeveledSpaceCellGetter item)
+        {
+            WriteBinaryChanceNoneCustom(
+                writer: writer,
+                item: item);
         }
 
         public void Write(
@@ -1722,8 +1734,10 @@ namespace Mutagen.Bethesda.Starfield
             {
                 case RecordTypeInts.LVLD:
                 {
-                    frame.Position += frame.MetaData.Constants.SubConstants.HeaderLength;
-                    item.ChanceNone = FloatBinaryTranslation<MutagenFrame, MutagenWriter>.Instance.Parse(reader: frame.SpawnWithLength(contentLength));
+                    LeveledSpaceCellBinaryCreateTranslation.FillBinaryChanceNoneCustom(
+                        frame: frame.SpawnWithLength(frame.MetaData.Constants.SubConstants.HeaderLength + contentLength),
+                        item: item,
+                        lastParsed: lastParsed);
                     return (int)LeveledSpaceCell_FieldIndex.ChanceNone;
                 }
                 case RecordTypeInts.LVLM:
@@ -1771,6 +1785,11 @@ namespace Mutagen.Bethesda.Starfield
                         translationParams: translationParams.WithNoConverter());
             }
         }
+
+        public static partial void FillBinaryChanceNoneCustom(
+            MutagenFrame frame,
+            ILeveledSpaceCellInternal item,
+            PreviousParse lastParsed);
 
     }
 
@@ -1820,8 +1839,12 @@ namespace Mutagen.Bethesda.Starfield
 
 
         #region ChanceNone
-        private int? _ChanceNoneLocation;
-        public Single ChanceNone => _ChanceNoneLocation.HasValue ? HeaderTranslation.ExtractSubrecordMemory(_recordData, _ChanceNoneLocation.Value, _package.MetaData.Constants).Float() : default(Single);
+        partial void ChanceNoneCustomParse(
+            OverlayStream stream,
+            int finalPos,
+            int offset);
+        public partial Single GetChanceNoneCustom();
+        public Single ChanceNone => GetChanceNoneCustom();
         #endregion
         #region MaxCount
         private int? _MaxCountLocation;
@@ -1907,7 +1930,10 @@ namespace Mutagen.Bethesda.Starfield
             {
                 case RecordTypeInts.LVLD:
                 {
-                    _ChanceNoneLocation = (stream.Position - offset);
+                    ChanceNoneCustomParse(
+                        stream: stream,
+                        finalPos: finalPos,
+                        offset: offset);
                     return (int)LeveledSpaceCell_FieldIndex.ChanceNone;
                 }
                 case RecordTypeInts.LVLM:
