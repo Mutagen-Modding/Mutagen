2a4edfb0b48e467d86fc2d8447b9b97e9e6658eb
Justin Swanson <justin.c.swanson@gmail.com>
2024-06-03 00:49:27 -0500

    RecordCompactionCompatibilityDetector

---
+++ Mutagen.Bethesda.Core/Plugins/Analysis/DI/RecordCompactionCompatibilityDetector.cs
@@ -0,0 +1,24 @@
+﻿using Mutagen.Bethesda.Plugins.Records;
+
+namespace Mutagen.Bethesda.Plugins.Analysis.DI;
+
+public class RecordCompactionCompatibilityDetector
+{
+    private const uint LightMasterMin = 0x800;
+    private const uint LightMasterMax = 0xFFF;
+    
+    public bool IsLightModCompatible(IModGetter modGetter)
+    {
+        if (!modGetter.CanBeLightMaster) return false;
+        ModKey patchModKey = modGetter.ModKey;
+
+        foreach (var rec in modGetter.EnumerateMajorRecords())
+        {
+            if (!rec.FormKey.ModKey.Equals(patchModKey)) continue;
+            if (rec.FormKey.ID < LightMasterMin) return false;
+            if (rec.FormKey.ID > LightMasterMax) return false;
+        }
+
+        return true;
+    }
+}
---
+++ Mutagen.Bethesda.UnitTests/Plugins/Analysis/RecordCompactionCompatibilityDetectorTests.cs
@@ -0,0 +1,72 @@
+﻿using FluentAssertions;
+using Mutagen.Bethesda.Oblivion;
+using Mutagen.Bethesda.Plugins;
+using Mutagen.Bethesda.Plugins.Analysis.DI;
+using Mutagen.Bethesda.Skyrim;
+using Mutagen.Bethesda.Testing.AutoData;
+using Xunit;
+using Npc = Mutagen.Bethesda.Skyrim.Npc;
+
+namespace Mutagen.Bethesda.UnitTests.Plugins.Analysis;
+
+public class RecordCompactionCompatibilityDetectorTests
+{
+    [Theory, MutagenModAutoData(GameRelease.Oblivion)]
+    public void OldGame(
+        OblivionMod mod,
+        RecordCompactionCompatibilityDetector sut)
+    {
+        sut.IsLightModCompatible(mod)
+            .Should().BeFalse();
+    }
+    
+    [Theory, MutagenModAutoData(GameRelease.SkyrimSE)]
+    public void LightMasterEmpty(
+        SkyrimMod mod,
+        RecordCompactionCompatibilityDetector sut)
+    {
+        sut.IsLightModCompatible(mod)
+            .Should().BeTrue();
+    }
+    
+    [Theory, MutagenModAutoData(GameRelease.SkyrimSE)]
+    public void LightMasterLowerFormIDCompatible(
+        SkyrimMod mod,
+        Npc n,
+        RecordCompactionCompatibilityDetector sut)
+    {
+        sut.IsLightModCompatible(mod)
+            .Should().BeTrue();
+    }
+    
+    [Theory, MutagenModAutoData(GameRelease.SkyrimSE)]
+    public void LightMasterLowerFormIDIncompatible(
+        SkyrimMod mod,
+        Npc n,
+        RecordCompactionCompatibilityDetector sut)
+    {
+        mod.ModHeader.Version = 55;
+        sut.IsLightModCompatible(mod)
+            .Should().BeFalse();
+    }
+    
+    [Theory, MutagenModAutoData(GameRelease.SkyrimSE)]
+    public void LightMasterNormalFormID(
+        SkyrimMod mod,
+        RecordCompactionCompatibilityDetector sut)
+    {
+        mod.Npcs.Add(new Npc(new FormKey(mod.ModKey, 0x801), SkyrimRelease.SkyrimSE));
+        sut.IsLightModCompatible(mod)
+            .Should().BeTrue();
+    }
+    
+    [Theory, MutagenModAutoData(GameRelease.SkyrimSE)]
+    public void LightMasterNotCompatible(
+        SkyrimMod mod,
+        RecordCompactionCompatibilityDetector sut)
+    {
+        mod.Npcs.Add(new Npc(new FormKey(mod.ModKey, 0xFFFF), SkyrimRelease.SkyrimSE));
+        sut.IsLightModCompatible(mod)
+            .Should().BeFalse();
+    }
+}
