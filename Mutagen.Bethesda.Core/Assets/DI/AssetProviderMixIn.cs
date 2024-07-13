0e8181470e825b4aaca6ccd0d6ba14941a6746e3
Elscrux <nickposer2102@gmail.com>
2024-07-13 17:45:21 +0200

    Extract default methods as extension methods

---
+++ Mutagen.Bethesda.Core/Assets/DI/AssetProviderMixIn.cs
@@ -0,0 +1,60 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using Mutagen.Bethesda.Plugins.Assets;
+namespace Mutagen.Bethesda.Assets.DI;
+
+public static class AssetProviderMixIn
+{
+    public static bool Exists(this IAssetProvider assetProvider, IAssetLinkGetter assetLink)
+    {
+        return assetProvider.Exists(assetLink.AssetPath);
+    }
+
+    public static bool TryGetStream(
+        this IAssetProvider assetProvider,
+        IAssetLinkGetter assetPath,
+        [MaybeNullWhen(false)] out Stream stream)
+    {
+        return assetProvider.TryGetStream(assetPath.AssetPath, out stream);
+    }
+
+    /// <summary>
+    /// Gets a stream for the asset path in the context of the asset provider
+    /// </summary>
+    /// <param name="assetPath">Asset path</param>
+    /// <returns>Resulting stream</returns>
+    /// <exception cref="FileNotFoundException">Thrown if the asset path does not exist in the context of the asset provider</exception>
+    public static Stream GetStream(this IAssetProvider assetProvider, AssetPath assetPath)
+    {
+        return assetProvider.TryGetStream(assetPath, out var stream)
+            ? stream
+            : throw new FileNotFoundException($"Asset not found: {assetPath}");
+    }
+
+    public static Stream GetStream(this IAssetProvider assetProvider, IAssetLinkGetter assetLink)
+    {
+        return assetProvider.GetStream(assetLink.AssetPath);
+    }
+
+    public static bool TryGetSize(this IAssetProvider assetProvider, IAssetLinkGetter assetPath, out uint size)
+    {
+        return assetProvider.TryGetSize(assetPath.AssetPath, out size);
+    }
+
+    /// <summary>
+    /// Gets the size of the asset path in the context of the asset provider
+    /// </summary>
+    /// <param name="assetPath">Asset path</param>
+    /// <returns>Resulting size in bytes</returns>
+    /// <exception cref="FileNotFoundException">Thrown if the asset path does not exist in the context of the asset provider</exception>
+    public static uint GetSize(this IAssetProvider assetProvider, AssetPath assetPath)
+    {
+        return assetProvider.TryGetSize(assetPath, out var size)
+            ? size
+            : throw new FileNotFoundException($"Asset not found: {assetPath}");
+    }
+
+    public static uint GetSize(this IAssetProvider assetProvider, IAssetLinkGetter assetLink)
+    {
+        return assetProvider.GetSize(assetLink.AssetPath);
+    }
+}
