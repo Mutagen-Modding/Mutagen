561db178871b972286b97654ffaeb9d3300f4bda
Justin Swanson <justin.c.swanson@gmail.com>
2024-11-17 22:20:30 -0600

    Source generator testing swapped to Verify

---
+++ Mutagen.Bethesda.UnitTests/SourceGenerators/CustomAspectGeneratorTests.TestSourceGeneratorWithEnvironmentVariables#CustomAspectInterfaces.g.verified.cs
@@ -0,0 +1,35 @@
+﻿//HintName: CustomAspectInterfaces.g.cs
+using <global namespace>;
+
+namespace SomeNamespace
+{
+    #region Wrappers
+    public class IArmorWrapper : ISomeCustomAspect
+    {
+        private readonly IArmor _wrapped;
+        public IFormLinkNullable<ISoundDescriptorGetter> PickUpSound
+        {
+            get => _wrapped.PickUpSound;
+        }
+
+        public IArmorWrapper(IArmor rhs)
+        {
+            _wrapped = rhs;
+        }
+    }
+
+
+    #endregion
+
+    #region Mix Ins
+    public static class WrapperMixIns
+    {
+        public static IArmorWrapper AsISomeCustomAspect(this IArmor rhs)
+        {
+            return new IArmorWrapper(rhs);
+        }
+
+    }
+    #endregion
+
+}
