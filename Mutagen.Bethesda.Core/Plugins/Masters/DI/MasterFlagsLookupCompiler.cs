a432c7fa03094f47df6328d8d855595ae6db57d4
Justin Swanson <justin.c.swanson@gmail.com>
2024-11-23 03:31:34 -0600

    MasterFlagsLookupCompiler

---
+++ Mutagen.Bethesda.Core/Plugins/Masters/DI/MasterFlagsLookupCompiler.cs
@@ -0,0 +1,42 @@
+﻿using System.IO.Abstractions;
+using Mutagen.Bethesda.Environments.DI;
+using Mutagen.Bethesda.Plugins.Binary.Headers;
+using Mutagen.Bethesda.Plugins.Meta;
+using Mutagen.Bethesda.Plugins.Records;
+
+namespace Mutagen.Bethesda.Plugins.Masters.DI;
+
+public class MasterFlagsLookupCompiler
+{
+    private readonly IFileSystem _fileSystem;
+    private readonly IGameReleaseContext _gameReleaseContext;
+    private readonly IDataDirectoryProvider _dataDirectoryProvider;
+    
+    public MasterFlagsLookupCompiler(
+        IFileSystem fileSystem,
+        IGameReleaseContext gameReleaseContext,
+        IDataDirectoryProvider dataDirectoryProvider)
+    {
+        _fileSystem = fileSystem;
+        _gameReleaseContext = gameReleaseContext;
+        _dataDirectoryProvider = dataDirectoryProvider;
+    }
+
+    public Noggog.Cache<IModMasterStyledGetter, ModKey>? ConstructFor(ModPath path)
+    {
+        Noggog.Cache<IModMasterStyledGetter, ModKey>? masterFlagsLookup = null;
+        if (GameConstants.Get(_gameReleaseContext.Release).SeparateMasterLoadOrders)
+        {
+            var dataDirectoryPath = _dataDirectoryProvider.Path;
+            var header = ModHeaderFrame.FromPath(path, _gameReleaseContext.Release, _fileSystem);
+            masterFlagsLookup = new Noggog.Cache<IModMasterStyledGetter, ModKey>(x => x.ModKey);
+            foreach (var master in header.Masters(path.ModKey).Select(x => x.Master))
+            {
+                var otherPath = Path.Combine(dataDirectoryPath, master.FileName);
+                var otherHeader = ModHeaderFrame.FromPath(otherPath, _gameReleaseContext.Release, _fileSystem);
+                masterFlagsLookup.Add(new KeyedMasterStyle(master, otherHeader.MasterStyle));
+            }
+        }
+        return masterFlagsLookup;
+    }
+}
