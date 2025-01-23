e8c08d06e759d9e1fccb83e72fae40725895c5b7
Justin Swanson <justin.c.swanson@gmail.com>
2025-01-23 01:08:57 -0600

    GetApplicableArchivePathsTests to use MutagenContainerAutoData

--- Mutagen.Bethesda.Core.UnitTests/Archives/GetApplicableArchivePathsTests.cs
+++ Mutagen.Bethesda.Core.UnitTests/Archives/GetApplicableArchivePathsTests.cs
@@ -1,16 +1,12 @@
 using System.IO.Abstractions;
-using System.IO.Abstractions.TestingHelpers;
 using FluentAssertions;
 using Mutagen.Bethesda.Archives.DI;
 using Mutagen.Bethesda.Environments.DI;
-using Mutagen.Bethesda.Inis;
-using Mutagen.Bethesda.Inis.DI;
-using Mutagen.Bethesda.Installs.DI;
 using Mutagen.Bethesda.Plugins;
-using Mutagen.Bethesda.Plugins.Order.DI;
 using Mutagen.Bethesda.Testing;
+using Mutagen.Bethesda.Testing.AutoData;
+using Mutagen.Bethesda.Testing.Fakes;
 using Noggog;
-using Noggog.Testing.IO;
 using Xunit;
 using Path = System.IO.Path;
 
@@ -18,125 +14,101 @@ namespace Mutagen.Bethesda.UnitTests.Archives;
 
 public class GetApplicableArchivePathsTests
 {
-    private static readonly string MyDocumentsPath = $"{PathingUtil.DrivePrefix}{Path.Combine("MyDocuments")}";
     private const string SomeExplicitListingBsa = "SomeExplicitListing.bsa";
     private const string UnusedExplicitListingBsa = "SomeExplicitListing2.bsa";
     private const string SkyrimBsa = "Skyrim.bsa";
     private const string MyModBsa = "MyMod.bsa";
-    private static readonly string BaseFolder = $"{PathingUtil.DrivePrefix}{Path.Combine("Games", "Skyrim", "Data")}";
-
-    private IFileSystem GetFileSystem()
-    {
-        var path = new IniPathProvider(
-            new GameReleaseInjection(GameRelease.SkyrimSE),
-            new IniPathLookupInjection(MyDocumentsPath));
-        var fs = new MockFileSystem(new Dictionary<string, MockFileData>()
-        {
-            { path.Path, new MockFileData($@"[Archive]
-sResourceArchiveList={SomeExplicitListingBsa}, {UnusedExplicitListingBsa}") }
-        });
-        fs.Directory.CreateDirectory(BaseFolder);
-        return fs;
-    }
-
-    private GetApplicableArchivePaths GetClass(IFileSystem fs)
-    {
-        var gameReleaseInjection = new GameReleaseInjection(GameRelease.SkyrimSE);
-        var ext = new ArchiveExtensionProvider(gameReleaseInjection);
-        return new GetApplicableArchivePaths(
-            fs, 
-            new CheckArchiveApplicability(
-                ext),
-            new DataDirectoryInjection(BaseFolder),
-            ext,
-            new CachedArchiveListingDetailsProvider(
-                new LoadOrderListingsInjection(Array.Empty<ModKey>()),
-                new GetArchiveIniListings(
-                    fs,
-                    new IniPathProvider(
-                        gameReleaseInjection,
-                        new IniPathLookupInjection(MyDocumentsPath))),
-                new ArchiveNameFromModKeyProvider(
-                    gameReleaseInjection)));
-    }
-
+    
     #region No ModKey
     
-    [Fact]
-    public void NoModKey()
+    [Theory, MutagenContainerAutoData]
+    public void NoModKey(
+        IFileSystem fs,
+        ManualArchiveIniListings manualArchiveIniListings,
+        IDataDirectoryProvider dataDir,
+        GetApplicableArchivePaths sut)
     {
-        var fs = GetFileSystem();
-        fs.File.WriteAllText(Path.Combine(BaseFolder, MyModBsa), string.Empty);
-        fs.File.WriteAllText(Path.Combine(BaseFolder, SkyrimBsa), string.Empty);
-        fs.File.WriteAllText(Path.Combine(BaseFolder, SomeExplicitListingBsa), string.Empty);
-        var get = GetClass(fs);
-        var applicable = get.Get()
+        manualArchiveIniListings.SetTo(SomeExplicitListingBsa, UnusedExplicitListingBsa);
+        fs.File.WriteAllText(Path.Combine(dataDir.Path, MyModBsa), string.Empty);
+        fs.File.WriteAllText(Path.Combine(dataDir.Path, SkyrimBsa), string.Empty);
+        fs.File.WriteAllText(Path.Combine(dataDir.Path, SomeExplicitListingBsa), string.Empty);
+        var applicable = sut.Get()
             .ToArray();
         applicable.Should().StartWith(new FilePath(
-            Path.Combine(BaseFolder, SomeExplicitListingBsa)));
+            Path.Combine(dataDir.Path, SomeExplicitListingBsa)));
         applicable.Should().BeEquivalentTo(new FilePath[]
         {
-            Path.Combine(BaseFolder, SomeExplicitListingBsa),
-            Path.Combine(BaseFolder, SkyrimBsa),
-            Path.Combine(BaseFolder, MyModBsa)
+            Path.Combine(dataDir.Path, SomeExplicitListingBsa),
+            Path.Combine(dataDir.Path, SkyrimBsa),
+            Path.Combine(dataDir.Path, MyModBsa)
         });
     }
 
     #endregion
 
     #region GetApplicableArchivePaths
-    [Fact]
-    public void Empty()
+    [Theory, MutagenContainerAutoData]
+    public void Empty(
+        ManualArchiveIniListings manualArchiveIniListings,
+        GetApplicableArchivePaths sut)
     {
-        var fs = GetFileSystem();
-        var get = GetClass(fs);
-        get.Get(TestConstants.Skyrim)
+        manualArchiveIniListings.SetTo(SomeExplicitListingBsa, UnusedExplicitListingBsa);
+        sut.Get(TestConstants.Skyrim)
             .Should().BeEmpty();
     }
 
-    [Fact]
-    public void NullModKey()
+    [Theory, MutagenContainerAutoData]
+    public void NullModKey(
+        IFileSystem fs,
+        ManualArchiveIniListings manualArchiveIniListings,
+        IDataDirectoryProvider dataDir,
+        GetApplicableArchivePaths sut)
     {
-        var fs = GetFileSystem();
-        var get = GetClass(fs);
-        fs.File.WriteAllText(Path.Combine(BaseFolder, SkyrimBsa), string.Empty);
-        fs.File.WriteAllText(Path.Combine(BaseFolder, SomeExplicitListingBsa), string.Empty);
-        fs.File.WriteAllText(Path.Combine(BaseFolder, MyModBsa), string.Empty);
-        var applicable = get.Get(ModKey.Null)
+        manualArchiveIniListings.SetTo(SomeExplicitListingBsa, UnusedExplicitListingBsa);
+        fs.File.WriteAllText(Path.Combine(dataDir.Path, SkyrimBsa), string.Empty);
+        fs.File.WriteAllText(Path.Combine(dataDir.Path, SomeExplicitListingBsa), string.Empty);
+        fs.File.WriteAllText(Path.Combine(dataDir.Path, MyModBsa), string.Empty);
+        var applicable = sut.Get(ModKey.Null)
             .Should().BeEmpty();
     }
 
-    [Fact]
-    public void BaseMod()
+    [Theory, MutagenContainerAutoData]
+    public void BaseMod(
+        IFileSystem fs,
+        ManualArchiveIniListings manualArchiveIniListings,
+        IDataDirectoryProvider dataDir,
+        GetApplicableArchivePaths sut)
     {
-        var fs = GetFileSystem();
-        var get = GetClass(fs);
-        fs.File.WriteAllText(Path.Combine(BaseFolder, SkyrimBsa), string.Empty);
-        fs.File.WriteAllText(Path.Combine(BaseFolder, SomeExplicitListingBsa), string.Empty);
-        fs.File.WriteAllText(Path.Combine(BaseFolder, MyModBsa), string.Empty);
-        var applicable = get.Get(TestConstants.Skyrim)
+        manualArchiveIniListings.SetTo(SomeExplicitListingBsa, UnusedExplicitListingBsa);
+        fs.File.WriteAllText(Path.Combine(dataDir.Path, SkyrimBsa), string.Empty);
+        fs.File.WriteAllText(Path.Combine(dataDir.Path, SomeExplicitListingBsa), string.Empty);
+        fs.File.WriteAllText(Path.Combine(dataDir.Path, MyModBsa), string.Empty);
+        var applicable = sut.Get(TestConstants.Skyrim)
             .ToArray();
         applicable.Should().Equal(new FilePath[]
         {
-            Path.Combine(BaseFolder, SomeExplicitListingBsa),
-            Path.Combine(BaseFolder, SkyrimBsa),
+            Path.Combine(dataDir.Path, SomeExplicitListingBsa),
+            Path.Combine(dataDir.Path, SkyrimBsa),
         });
     }
 
-    [Fact]
-    public void Typical()
+    [Theory, MutagenContainerAutoData]
+    public void Typical(
+        IFileSystem fs,
+        ManualArchiveIniListings manualArchiveIniListings,
+        IDataDirectoryProvider dataDir,
+        GetApplicableArchivePaths sut)
     {
-        var fs = GetFileSystem();
-        var get = GetClass(fs);
-        fs.File.WriteAllText(Path.Combine(BaseFolder, $"{TestConstants.MasterModKey2.Name}.bsa"), string.Empty);
-        fs.File.WriteAllText(Path.Combine(BaseFolder, SomeExplicitListingBsa), string.Empty);
-        fs.File.WriteAllText(Path.Combine(BaseFolder, MyModBsa), string.Empty);
-        var applicable = get.Get(TestConstants.MasterModKey2)
+        manualArchiveIniListings.SetTo(SomeExplicitListingBsa, UnusedExplicitListingBsa);
+        fs.File.WriteAllText(Path.Combine(dataDir.Path, $"{TestConstants.MasterModKey2.Name}.bsa"), string.Empty);
+        fs.File.WriteAllText(Path.Combine(dataDir.Path, SomeExplicitListingBsa), string.Empty);
+        fs.File.WriteAllText(Path.Combine(dataDir.Path, MyModBsa), string.Empty);
+        var applicable = sut.Get(TestConstants.MasterModKey2)
             .ToArray();
         applicable.Should().Equal(new FilePath[]
         {
-            Path.Combine(BaseFolder, SomeExplicitListingBsa),
-            Path.Combine(BaseFolder, $"{TestConstants.MasterModKey2.Name}.bsa")
+            Path.Combine(dataDir.Path, SomeExplicitListingBsa),
+            Path.Combine(dataDir.Path, $"{TestConstants.MasterModKey2.Name}.bsa")
         });
     }
     #endregion
