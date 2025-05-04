3405dfd8c07d96d557e36b354df6f843f3d535af
Justin Swanson <justin.c.swanson@gmail.com>
2025-05-04 00:18:11 -0500

    Moved FontProvider within DI namespace

--- Mutagen.Bethesda.Core/Fonts/DI/FontProvider.cs
+++ Mutagen.Bethesda.Core/Fonts/DI/FontProvider.cs
@@ -1,40 +1,14 @@
 ﻿using Mutagen.Bethesda.Assets;
-using Mutagen.Bethesda.Fonts.DI;
 using Mutagen.Bethesda.Strings;
 
-namespace Mutagen.Bethesda.Fonts;
-
-public interface IFontProvider {
-	/// <summary>
-	/// Gets the data relative file paths of the font libraries
-	/// </summary>
-	IReadOnlyList<DataRelativePath> FontLibraries { get; }
-
-	/// <summary>
-	/// Gets the font mappings
-	/// <example>
-	/// "$HandwrittenFont" => "SkyrimBooks_Handwritten_Bold"
-	/// </example>
-	/// </summary>
-	IReadOnlyDictionary<string, FontMapping> FontMappings { get; }
-
-	/// <summary>
-	/// Gets the valid characters for names
-	/// </summary>
-	IReadOnlyList<char> ValidNameChars { get; }
-
-	/// <summary>
-	/// Gets the valid characters for books
-	/// </summary>
-	IReadOnlyList<char> ValidBookChars { get; }
-}
+namespace Mutagen.Bethesda.Fonts.DI;
 
 public class FontProvider : IFontProvider 
 {
 	private readonly List<DataRelativePath> _fontLibraries = new();
 	private readonly Dictionary<string, FontMapping> _fontMappings = new();
-	private char[] _validNameChars = Array.Empty<char>();
-	private char[] _validBookChars = Array.Empty<char>();
+	private char[] _validNameChars = [];
+	private char[] _validBookChars = [];
 
 	public IReadOnlyList<DataRelativePath> FontLibraries => _fontLibraries;
 	public IReadOnlyDictionary<string, FontMapping> FontMappings => _fontMappings;
