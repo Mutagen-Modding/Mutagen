f062429d44e55284d493c1e5fe6bb19a124f9bef
Elscrux <nickposer2102@gmail.com>
2026-06-19 02:22:37 +0200

    Add variants of location extensions which look at the load order to merge the states of special fields in the location record which are also merged at runtime

--- Mutagen.Bethesda.Skyrim/Extensions/LocationExt.cs
+++ Mutagen.Bethesda.Skyrim/Extensions/LocationExt.cs
@@ -1,4 +1,6 @@
 ﻿using Mutagen.Bethesda.Plugins;
+using Mutagen.Bethesda.Plugins.Cache;
+using Noggog;
 namespace Mutagen.Bethesda.Skyrim;
 
 public static class LocationExt
@@ -33,6 +35,41 @@ public static class LocationExt
             .Where(x => !removed.Contains(x.Ref.FormKey));
     }
 
+    public static IEnumerable<ILocationRefTypeReferenceGetter> AllLocationRefTypesReferences(this ILocationGetter location, ILinkCache linkCache)
+    {
+        var previousOverrides = linkCache
+            .ResolveAll(location, ResolveTarget.Origin)
+            .TakeWhile(x => !Equals(x, location))
+            .Append(location)
+            .ToArray();
+
+        var refTypes = new HashSet<ILocationRefTypeReferenceGetter>();
+        foreach (var previousOverride in previousOverrides)
+        {
+            refTypes.Add(Added(previousOverride));
+            var removed = Removed(previousOverride);
+            refTypes.RemoveWhere(x => removed.Any(r => r.FormKey == x.Ref.FormKey));
+        }
+        return refTypes;
+
+        IEnumerable<ILocationRefTypeReferenceGetter> Added(ILocationGetter loc)
+        {
+            var referencesStatic = loc.LocationRefTypeReferencesStatic;
+            if (referencesStatic is not null) return referencesStatic;
+
+            var typeReferencesAdded = loc.LocationRefTypeReferencesAdded;
+            return typeReferencesAdded ?? [];
+        }
+
+        IReadOnlyList<IFormLinkGetter<IPlacedSimpleGetter>> Removed(ILocationGetter loc)
+        {
+            var referencesRemoved = loc.LocationRefTypeReferencesRemoved;
+            if (referencesRemoved is not null) return referencesRemoved;
+
+            return [];
+        }
+    }
+
     public static IEnumerable<IPersistentActorReferenceGetter> PersistentActorReferences(this ILocationGetter location)
     {
         IEnumerable<IPersistentActorReferenceGetter> Added()
@@ -61,6 +98,41 @@ public static class LocationExt
 
         return Added()
             .Where(x => !removed.Contains(x.Actor.FormKey));
+    }
+
+    public static HashSet<IPersistentActorReferenceGetter> AllPersistentActorReferences(this ILocationGetter location, ILinkCache linkCache)
+    {
+        var previousOverrides = linkCache
+            .ResolveAll(location, ResolveTarget.Origin)
+            .TakeWhile(x => !Equals(x, location))
+            .Append(location)
+            .ToArray();
+
+        var actors = new HashSet<IPersistentActorReferenceGetter>();
+        foreach (var previousOverride in previousOverrides)
+        {
+            actors.Add(Added(previousOverride));
+            var removed = Removed(previousOverride);
+            actors.RemoveWhere(x => removed.Any(r => r.FormKey == x.Actor.FormKey));
+        }
+        return actors;
+
+        IEnumerable<IPersistentActorReferenceGetter> Added(ILocationGetter loc)
+        {
+            var referencesStatic = loc.PersistentActorReferencesStatic;
+            if (referencesStatic is not null) return referencesStatic;
+
+            var typeReferencesAdded = loc.PersistentActorReferencesAdded;
+            return typeReferencesAdded ?? [];
+        }
+
+        IReadOnlyList<IFormLinkGetter<IPlacedSimpleGetter>> Removed(ILocationGetter loc)
+        {
+            var referencesRemoved = loc.PersistentActorReferencesRemoved;
+            if (referencesRemoved is not null) return referencesRemoved;
+
+            return [];
+        }
     }
 
     public static IEnumerable<IUniqueActorReferenceGetter> UniqueActorReferences(this ILocationGetter location)
@@ -93,6 +165,41 @@ public static class LocationExt
             .Where(x => !removed.Contains(x.Actor.FormKey));
     }
 
+    public static HashSet<IUniqueActorReferenceGetter> AllUniqueActorReferences(this ILocationGetter location, ILinkCache linkCache)
+    {
+        var previousOverrides = linkCache
+            .ResolveAll(location, ResolveTarget.Origin)
+            .TakeWhile(x => !Equals(x, location))
+            .Append(location)
+            .ToArray();
+
+        var actors = new HashSet<IUniqueActorReferenceGetter>();
+        foreach (var previousOverride in previousOverrides)
+        {
+            actors.Add(Added(previousOverride));
+            var removed = Removed(previousOverride);
+            actors.RemoveWhere(x => removed.Any(r => r.FormKey == x.Actor.FormKey));
+        }
+        return actors;
+
+        IReadOnlyList<IUniqueActorReferenceGetter> Added(ILocationGetter loc)
+        {
+            var referencesStatic = loc.UniqueActorReferencesStatic;
+            if (referencesStatic is not null) return referencesStatic;
+
+            var typeReferencesAdded = loc.UniqueActorReferencesAdded;
+            return typeReferencesAdded ?? [];
+        }
+
+        IReadOnlyList<IFormLinkGetter<INpcGetter>> Removed(ILocationGetter loc)
+        {
+            var referencesRemoved = loc.UniqueActorReferencesRemoved;
+            if (referencesRemoved is not null) return referencesRemoved;
+
+            return [];
+        }
+    }
+
     public static IEnumerable<IFormLinkGetter<IPlacedGetter>> InitiallyDisabledReferences(this ILocationGetter location)
     {
         var staticReferences = location.InitiallyDisabledReferencesAdded;
@@ -114,6 +221,31 @@ public static class LocationExt
         }
     }
 
+    public static HashSet<IFormLinkGetter<IPlacedGetter>> AllInitiallyDisabledReferences(this ILocationGetter location, ILinkCache linkCache)
+    {
+        var previousOverrides = linkCache
+            .ResolveAll(location, ResolveTarget.Origin)
+            .TakeWhile(x => !Equals(x, location))
+            .Append(location)
+            .ToArray();
+
+        var actors = new HashSet<IFormLinkGetter<IPlacedGetter>>();
+        foreach (var previousOverride in previousOverrides)
+        {
+            actors.Add(Added(previousOverride));
+        }
+        return actors;
+
+        IReadOnlyList<IFormLinkGetter<IPlacedGetter>> Added(ILocationGetter loc)
+        {
+            var referencesStatic = loc.InitiallyDisabledReferencesStatic;
+            if (referencesStatic is not null) return referencesStatic;
+
+            var typeReferencesAdded = loc.InitiallyDisabledReferencesAdded;
+            return typeReferencesAdded ?? [];
+        }
+    }
+
     public static IEnumerable<IEnableParentReferenceGetter> EnableParentReferences(this ILocationGetter location)
     {
         var staticReferences = location.EnableParentReferencesStatic;
@@ -132,6 +264,31 @@ public static class LocationExt
             {
                 yield return reference;
             }
+        }
+    }
+
+    public static HashSet<IEnableParentReferenceGetter> AllEnableParentReferences(this ILocationGetter location, ILinkCache linkCache)
+    {
+        var previousOverrides = linkCache
+            .ResolveAll(location, ResolveTarget.Origin)
+            .TakeWhile(x => !Equals(x, location))
+            .Append(location)
+            .ToArray();
+
+        var actors = new HashSet<IEnableParentReferenceGetter>();
+        foreach (var previousOverride in previousOverrides)
+        {
+            actors.Add(Added(previousOverride));
+        }
+        return actors;
+
+        IReadOnlyList<IEnableParentReferenceGetter> Added(ILocationGetter loc)
+        {
+            var referencesStatic = loc.EnableParentReferencesStatic;
+            if (referencesStatic is not null) return referencesStatic;
+
+            var typeReferencesAdded = loc.EnableParentReferencesAdded;
+            return typeReferencesAdded ?? [];
         }
     }
 }
